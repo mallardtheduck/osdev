638a56956e1eb353cc82f789e3d4199b66a25092
stuartb <stuart@jadu.net>
2014-05-02 13:59:43 +0100

    Made serial debug locking non-blocking to reduce breakage with scheduler.

--- src/kernel/serdbg.cpp
+++ src/kernel/serdbg.cpp
@@ -18,8 +18,19 @@ void init_serial() {
 	serial_writestring("Serial debugging enabled.\n");
 }
 
+void reinit_serial() {
+	outb(PORT + 1, 0x00);    // Disable all interrupts
+	outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+	outb(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+	outb(PORT + 1, 0x00);    //                  (hi byte)
+	outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
+	outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+	outb(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+	serial_writestring("Serial debugging enabled.\n");
+}
+
 int is_transmit_empty() {
-	take_lock(ser_lock);
+	if(!try_take_lock(ser_lock))return -1;
 	int ret=inb(PORT + 5) & 0x20;
 	release_lock(ser_lock);
 	return ret;
@@ -27,7 +38,7 @@ int is_transmit_empty() {
 
 void write_serial(char a) {
 	while (is_transmit_empty() == 0);
-	take_lock(ser_lock);
+	if(!try_take_lock(ser_lock))return;
 	outb(PORT, a);
 	release_lock(ser_lock);
 	while (is_transmit_empty() == 0);
@@ -38,7 +49,7 @@ void serial_writestring(char *str){
 }
 
 int serial_received() {
-	take_lock(ser_lock);
+	if(!try_take_lock(ser_lock))return -1;
 	int ret=inb(PORT + 5) & 1;
 	release_lock(ser_lock);
 	return ret;
@@ -46,7 +57,7 @@ int serial_received() {
  
 char read_serial() {
 	while (serial_received() == 0);
-	take_lock(ser_lock);
+	if(!try_take_lock(ser_lock))return '\0';
 	char ret=inb(PORT);
 	release_lock(ser_lock);
 	return ret;
