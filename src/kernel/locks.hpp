f597858cb534b2a72f92b0e703f99d6f1c447904
stuartb <stuart@jadu.net>
2014-05-06 15:53:10 +0100

    Added RAII hold_lock class.

--- src/kernel/locks.hpp
+++ src/kernel/locks.hpp
@@ -34,4 +34,12 @@ inline void release_lock(lock &l, uint64_t thread=sch_get_id()){
 	}
 }
 
+class hold_lock{
+private:
+	lock *l;
+public:
+	hold_lock(lock &lck): l(&lck) {take_lock(*l);}
+	~hold_lock() { release_lock(*l);}
+};
+
 #endif
