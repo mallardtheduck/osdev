032356bcbd84bc3e9687ff349e7c096c9995c58d
stuartb <stuart@jadu.net>
2014-05-07 11:47:22 +0100

    Re-arranged order of initialization.

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -6,21 +6,20 @@ extern "C" void kernel_main(multiboot_info_t *mbd, unsigned int magic)
 		panic("Interrupts not disabled at kernel load!\n");
 	}
 	init_serial();
-	GDT_init();
-	IDT_init();
 	terminal_initialize();
 	printf("%s %s\n%s\n", KERNEL_OS_NAME, KERNEL_VERSION_STRING, KERNEL_COPYRIGHT);
+	GDT_init();
+	IDT_init();
 	init_cpu();
 	mm_init(mbd);
 	disable_pic();
 	PIC_init();
 	enable_interrupts();
-	fs_init();
 	sch_init();
 	drv_init();
+	fs_init();
 	printf("Ready.");
 	//while(true) asm volatile("hlt");
-	asm("int $128");
 	sch_block();
 	panic("Kernel endpoint reached!\n");
 }
