1cd3cf5defeec02b3a7b9919bdb6a4bdb49a8d8d
Mallard <stuartbrockman@gmail.com>
2014-05-10 23:14:42 +0100

    Reverted to inline code for locks.

--- src/kernel/locks.hpp
+++ src/kernel/locks.hpp
@@ -3,22 +3,39 @@
 
 #include "kernel.hpp"
 
+
 typedef volatile uint64_t lock;
 
-void init_lock(lock &l);
 
-void take_lock(lock &l, uint64_t thread=sch_get_id());
+inline void init_lock(lock &l){
+	l=0;
+}
 
-bool try_take_lock(lock &l, uint64_t thread=sch_get_id());
 
-void release_lock(lock &l, uint64_t thread=sch_get_id());
+inline void take_lock(lock &l, uint64_t thread=sch_get_id()){
+	while(!__sync_bool_compare_and_swap(&l, 0, thread)) sch_yield();
+}
+
+
+inline bool try_take_lock(lock &l, uint64_t thread=sch_get_id()){
+	return __sync_bool_compare_and_swap(&l, 0, thread);
+}
+
+
+inline void release_lock(lock &l, uint64_t thread=sch_get_id()){
+	if(!__sync_bool_compare_and_swap(&l, thread, 0)){
+		panic("(LOCK) Attempt to release lock that isn't held!\n");
+	}
+}
+
 
 class hold_lock{
 private:
 	lock *l;
+
 public:
 	hold_lock(lock &lck): l(&lck) {take_lock(*l);}
-	~hold_lock() {release_lock(*l);}
+	~hold_lock() { release_lock(*l);}
 };
 
 #endif
