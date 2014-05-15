00f894b6cdfeb96a7f134e6f5348267281b154ee
stuartb <stuart@jadu.net>
2014-05-15 10:59:58 +0100

    Test module now takes syscall vector parameter.

--- src/modules/test/test.c
+++ src/modules/test/test.c
@@ -1,8 +1,16 @@
+#include <stddef.h>
+#define SYS_OUTSTRING 0x01
+
+typedef int (*syscall_vector)(int, void*);
+syscall_vector syscall;
+
 int getthenumber(){
 	return 42;
 }
 
-int module_main(){
+int module_main(syscall_vector sys){
+	syscall=sys;
 	int thenumber=getthenumber();
+	syscall(SYS_OUTSTRING, (void*)"Hello world!\n");
 	return thenumber*2;
 }
