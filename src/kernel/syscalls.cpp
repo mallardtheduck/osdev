1efc0459fea19d8d3a2b741bd1a772553512242b
stuartb <stuartbrockman@gmail.com>
2014-05-28 13:55:48 +0100

    Threading functions in module api now implemented.

--- src/kernel/syscalls.cpp
+++ src/kernel/syscalls.cpp
@@ -49,6 +49,29 @@ int module_syscall(int fn, void *p){
 	case module_api::SYS_DBGOUT:{
 		dbgout((char*)p);
 		}break;
+	case module_api::SYS_NEWTHREAD:{
+		struct nt_p{void(*entry)(void*); void *param;};
+		nt_p &nt_pa=*(nt_p*)p;
+		sch_new_thread(nt_pa.entry, nt_pa.param);
+		}break;
+	case module_api::SYS_BLOCK:{
+		sch_block();
+		}break;
+	case module_api::SYS_YIELD:{
+		sch_yield();
+		}break;
+	case module_api::SYS_THREADID:{
+		return (uint32_t)&sch_get_id();
+		}break;
+	case module_api::SYS_PRIORITY:{
+		sch_set_priority((uint32_t)p);
+		}break;
+	case module_api::SYS_ENDTHREAD:{
+		sch_end_thread();
+		}break;
+	case module_api::SYS_UNBLOCK:{
+		//TODO:Implement
+		}break;
 	}
 	return 0;
 }
