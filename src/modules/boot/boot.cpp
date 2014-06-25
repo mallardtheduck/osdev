abc411a8a6eb25db197ca12594875404447c6625
stuartb <stuartbrockman@gmail.com>
2014-06-25 15:36:22 +0100

    Boot manager starting to exist...

--- src/modules/boot/boot.cpp
+++ src/modules/boot/boot.cpp
@@ -4,23 +4,56 @@ syscall_table *SYSCALL_TABLE;
 
 #include "ini.h"
 
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+int strlen(const char *s){
+	int i;
+	while(*s++) ++i;
+	return i;
+}
+
+char *strdup(const char *s){
+	char *ret=(char*)malloc(strlen(s));
+	strncpy(ret, s, strlen(s));
+	return ret;
+}
+
+void dputs(void *handle, char *c){
+	devwrite(handle, strlen(c), c);
+}
+
+struct config{
+	char *display;
+} c;
+
+void displaywrite(const char *s){
+	void *handle=devopen(c.display);
+    dputs(handle, (char*)s);
+    devclose(handle);
+}
+
 static int handler(void*, const char* section, const char* name, const char* value){
-	dbgout("BOOT: [");
-	dbgout((char*)section);
-	dbgout("] ");
-	dbgout((char*)name);
-	dbgout("=");
-	dbgout((char*)value);
-	dbgout("\n");
+	#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+
+	if(MATCH("default", "display")){
+		c.display=strdup(value);
+		displaywrite("Starting BT/OS...");
+	}else if(MATCH("default", "load")){
+		module_load((char*)value);
+	}
 	return 1;
+}
+
+void boot_thread(void*){
+	dbgout("BOOT: Boot manager loaded.\n");
+	if (ini_parse("INIT:/config.ini", handler, NULL) < 0) {
+            panic("(BOOT) Can't load 'config.ini'!\n");
+            return 1;
+    }
+	return 0;
 }
 
 extern "C" int module_main(syscall_table *systbl){
 	SYSCALL_TABLE=systbl;
-	dbgout("BOOT: Boot manager loaded.\n");
-	if (ini_parse("INIT:/config.ini", handler, NULL) < 0) {
-            dbgout("Can't load 'config.ini'\n");
-            return 1;
-        }
-	return 0;
+	new_thread(&boot_thread, NULL);
 }
