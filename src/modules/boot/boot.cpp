cfb7b75adb7c13bcb92fa4cb31895b4d3eded31c
stuartb <stuartbrockman@gmail.com>
2014-06-25 13:20:50 +0100

    Added ini parser to boot manager.

--- src/modules/boot/boot.cpp
+++ src/modules/boot/boot.cpp
@@ -2,8 +2,25 @@
 
 syscall_table *SYSCALL_TABLE;
 
+#include "ini.h"
+
+static int handler(void*, const char* section, const char* name, const char* value){
+	dbgout("BOOT: [");
+	dbgout((char*)section);
+	dbgout("] ");
+	dbgout((char*)name);
+	dbgout("=");
+	dbgout((char*)value);
+	dbgout("\n");
+	return 1;
+}
+
 extern "C" int module_main(syscall_table *systbl){
 	SYSCALL_TABLE=systbl;
 	dbgout("BOOT: Boot manager loaded.\n");
+	if (ini_parse("INIT:/config.ini", handler, NULL) < 0) {
+            dbgout("Can't load 'config.ini'\n");
+            return 1;
+        }
 	return 0;
 }
