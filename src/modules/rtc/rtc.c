46c4b478ad8e4a0c0a4d881a23f82b1d26ff3411
stuartb <stuartbrockman@gmail.com>
2014-07-15 14:58:06 +0100

    RTC driver now reads RTC values and stores in global environment variable.

--- src/modules/rtc/rtc.c
+++ src/modules/rtc/rtc.c
@@ -32,17 +32,57 @@ bool cmos_update_in_progress(){
 	return read_cmos(0x0A) & (1<<7);
 }
 
-int module_main(syscall_table *systbl){
-	SYSCALL_TABLE=systbl;
+struct datetime{
+	int day, month, year;
+	int hour, minute, second;
+};
+typedef struct datetime datetime;
+
+inline static int bcdtobin(int bcd){
+	return ( (bcd & 0xF0) >> 1) + ( (bcd & 0xF0) >> 3) + (bcd & 0xf);
+}
+
+datetime read_rtc(){
+	int status=read_cmos(0x0B);
+	bool rtc_bin=(status & 4);
+	bool rtc_24h=(status & 2);
+	bool rtc_pm=false;
+	dbgpf("RTC: 24-Hour:%i Binary:%i\n", (int)rtc_24h, (int)rtc_bin);
+	datetime ret;
 	while(!cmos_update_in_progress());
 	while(cmos_update_in_progress());
-	int seconds, minutes, hours, day, month, year;
-	seconds=read_cmos(0x00);
-	minutes=read_cmos(0x02);
-	hours=read_cmos(0x04);
-	day=read_cmos(0x07);
-	month=read_cmos(0x08);
-	year=read_cmos(0x09);
-	dbgpf("RTC: %i:%i:%i %i/%i/%i\n", hours, minutes, seconds, day, month, year);
+	ret.day=read_cmos(0x07);
+	ret.month=read_cmos(0x08);
+	ret.year=read_cmos(0x09);
+	ret.hour=read_cmos(0x04);
+	ret.minute=read_cmos(0x02);
+	ret.second=read_cmos(0x00);
+	if(!rtc_24h){
+		rtc_pm=(ret.hour & 0x80);
+		ret.hour-=(ret.hour & 0x80);
+	}
+	if(!rtc_bin){
+		ret.day=bcdtobin(ret.day);
+		ret.month=bcdtobin(ret.month);
+		ret.year=bcdtobin(ret.year);
+		ret.hour=bcdtobin(ret.hour);
+		ret.minute=bcdtobin(ret.minute);
+		ret.second=bcdtobin(ret.second);
+	}
+	if(rtc_pm){
+		ret.hour+=12;
+		if(ret.hour==24) ret.hour=12;
+	}
+	if(!rtc_24h && !rtc_pm && ret.hour==12) ret.hour=0;
+	return ret;
+}
+
+int module_main(syscall_table *systbl){
+	SYSCALL_TABLE=systbl;
+	datetime dt=read_rtc();
+	dbgpf("RTC: %i:%i:%i %i/%i/%i\n", dt.hour, dt.minute, dt.second, dt.day, dt.month, dt.year);
+	char buf[128];
+	sprintf(buf, "%i:%i:%i %i/%i/%i\n", dt.hour, dt.minute, dt.second, dt.day, dt.month, dt.year);
+	setenv("BOOT_TIME", buf, ENV_Global, 0);
 	return 0;
 }
