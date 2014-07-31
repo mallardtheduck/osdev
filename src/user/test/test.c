07e2f7d86a6dbfaeb068979793907d8e7e30267e
stuartb <stuartbrockman@gmail.com>
2014-07-31 17:04:07 +0100

    Added mount/unmount to test program.

--- src/user/test/test.c
+++ src/user/test/test.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv){
 	bt_zero("Hello world!\n");
 	void *q=bt_alloc_pages(1);
 	bt_free_pages(q, 1);
+	bt_mount("TEST", "NULL", "INITFS");
+	bt_unmount("TEST");
 	bt_zero("All done.\n");
 	btos_call(BT_EXIT, 0, 0, 0);
     return 0;
