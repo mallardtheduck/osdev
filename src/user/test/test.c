8346352696b46a732f0c165843b2dfa0a5bf3405
stuartb <stuartbrockman@gmail.com>
2014-07-31 13:15:04 +0100

    Changes to test program to use "zero" API.

--- src/user/test/test.c
+++ src/user/test/test.c
@@ -3,6 +3,8 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 int main(int argc, char **argv){
+	char *hello="Hello world!\n";
+	btos_call(0, (uint32_t)hello ,0 ,0);
 	uint32_t q=btos_call(BT_ALLOC_PAGES, 1, 0, 0);
 	btos_call(BT_FREE_PAGES, q, 1, 0);
 	btos_call(BT_EXIT, 0, 0, 0);
