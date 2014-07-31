fd9cdd475ccab58810db64d7298d33a05760a9cd
stuartb <stuartbrockman@gmail.com>
2014-07-31 10:16:28 +0100

    Update test application to use userspace API.

--- src/user/test/test.c
+++ src/user/test/test.c
@@ -1,6 +1,10 @@
+#include "../../include/btos_stubs.h"
+
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 int main(int argc, char **argv){
-	while(1) asm("int $0x80");
+	uint32_t q=btos_call(BT_ALLOC_PAGES, 1, 0, 0);
+	btos_call(BT_FREE_PAGES, q, 1, 0);
+	btos_call(BT_EXIT, 0, 0, 0);
     return 0;
 }
