eb7e1914b2f1b2b4a2353053cde6dc7363bd3fb8
stuartb <stuartbrockman@gmail.com>
2014-08-01 17:06:42 +0100

    Improvements to test program.

--- src/user/test/test.c
+++ src/user/test/test.c
@@ -2,6 +2,7 @@
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
+char stdout_device[255]={'D', 'E', 'V', ':', '/'};
 bt_filehandle stdout=0;
 
 size_t strlen(const char *s){
@@ -12,19 +13,21 @@ size_t strlen(const char *s){
 
 void print_string(const char *s){
 	if(!stdout){
+		bt_getenv("DISPLAY_DEVICE", &stdout_device[5], 250);
     	stdout=bt_fopen("DEV:/VGATEXT0", 0);
     }
     bt_fwrite(stdout, strlen(s), s);
 }
 
 int main(int argc, char **argv){
-	//bt_zero("Hello world!\n");
+	bt_zero("~~~Userspace test program start!~~~\n");
 	print_string("Hello world!\n");
 	void *q=bt_alloc_pages(1);
 	bt_free_pages(q, 1);
 	bt_mount("TEST", "NULL", "INITFS");
 	bt_unmount("TEST");
-	bt_zero("~~~Userspace test program done!~~~\n");
+
+	print_string("Directory listing of \"INIT:/\":\n");
 	bt_dirhandle dir=bt_dopen("INIT:/", 0);
 	directory_entry entry=bt_dread(dir);
 	while(entry.valid){
@@ -33,6 +36,17 @@ int main(int argc, char **argv){
 		entry=bt_dread(dir);
 	}
 	bt_dclose(dir);
+
+	print_string("Contents of \"INIT:/config.ini\":\n");
+	bt_filehandle file=bt_fopen("INIT:/config.ini", 0);
+	char c;
+	while(bt_fread(file, 1, &c)){
+		char d[2]={c, 0};
+		print_string(d);
+	}
+	bt_fclose(file);
+
+	bt_zero("~~~Userspace test program done!~~~\n");
 	btos_call(BT_EXIT, 0, 0, 0);
     return 0;
 }
