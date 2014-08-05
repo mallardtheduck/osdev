67719e856245dc5be04001f31b98a226054419e0
stuartb <stuartbrockman@gmail.com>
2014-08-05 12:49:54 +0100

    Don't attempt to setblock while the current thread is holding the scheduler lock. This probably means we're in the scheduler.

--- src/kernel/locks.hpp
+++ src/kernel/locks.hpp
@@ -17,7 +17,7 @@ inline void take_lock(lock &l, uint64_t thread=sch_get_id()){
 	if(!sch_active()) return;
 	if(l==thread && thread!=0) panic("(LOCK) Attempt to take lock that's already held!\n");
 	while(!__sync_bool_compare_and_swap(&l, 0, thread)){
-		if(&l!=&sch_lock) sch_setblock(&lock_blockcheck, (void*)&l);
+		if(&l!=&sch_lock && sch_lock!=thread) sch_setblock(&lock_blockcheck, (void*)&l);
 	}
 }
 
