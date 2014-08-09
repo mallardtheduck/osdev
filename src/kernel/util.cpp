c9329b28b01fe91d48c4f18fb208fa9ae5273d2b
Stuart Brockman <stuartbrockman@gmail.com>
2014-08-09 11:43:10 +0100

    Correctly consider empty strings as equal.

--- src/kernel/util.cpp
+++ src/kernel/util.cpp
@@ -66,6 +66,7 @@ void *memset(void *s, int c, size_t n)
 
 int strcmp(const char *s1, const char *s2)
 {
+	if(!*s1 && !*s2) return 0;
 	while((*s1 && *s2) && (*s1++ == *s2++));
 	if(!*s1 != !*s2) return *s1 - *s2;
 	else return *(--s1) - *(--s2);
