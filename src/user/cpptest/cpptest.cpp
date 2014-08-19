930bb4ef54d7f2b70525081a056b9140d0f5d261
stuartb <stuartbrockman@gmail.com>
2014-08-19 10:52:29 +0100

    Enhancements to C++ test program.

--- src/user/cpptest/cpptest.cpp
+++ src/user/cpptest/cpptest.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
-int main(){
+std::vector<std::string> args2vector(int argc, char **argv){
+	std::vector<std::string> ret;
+	for(int i=0; i<argc; ++i){
+		ret.push_back(argv[i]);
+	}
+	return ret;
+}
+
+int main(int argc, char **argv){
 	std::cout << "Hello world!" << std::endl;
+	std::vector<std::string> args=args2vector(argc, argv);
+	for(auto i=args.begin(); i!=args.end(); ++i){
+		std::cout << *i << std::endl;
+	}
+	std::ifstream file("info:/procs");
+	while(!file.eof()){
+		std::string line;
+		getline(file, line);
+		std::cout << line << std::endl;
+	}
 	return 0;
 }
