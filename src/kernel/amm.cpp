49c24fac0a2094b89a089d4def036fa1aa445b0f
mallard <stuartbrockman@gmail.com>
2014-08-30 11:58:23 +0100

    AMM now uses custom allocator to work around re-entrancy issues.

--- src/kernel/amm.cpp
+++ src/kernel/amm.cpp
@@ -8,12 +8,74 @@ struct amm_pagedetails{
 	void *ptr;
 };
 
-typedef map<uint32_t, amm_pagedetails> amm_alloc_map;
+extern lock vmm_lock, sch_lock;
+
+template<typename T> class amm_allocator {
+public:
+typedef T* pointer;
+	typedef T& reference;
+	typedef const T* const_pointer;
+	typedef const T& const_reference;
+	typedef T value_type;
+
+	pointer allocate (size_t n, void* hint=0){
+		size_t pages=((n*sizeof(value_type))/VMM_PAGE_SIZE)+1;
+		take_lock(sch_lock);
+		bool lock_vmm=true;
+		if(vmm_lock==sch_get_id()){
+			release_lock(vmm_lock);
+		}else{
+			lock_vmm=false;
+			release_lock(sch_lock);
+			take_lock(vmm_lock);
+			take_lock(sch_lock);
+			release_lock(vmm_lock);
+		}
+		pointer ret=(pointer)vmm_alloc(pages);
+		if(lock_vmm) take_lock(vmm_lock);
+		release_lock(sch_lock);
+		return ret;
+	}
+	void deallocate (pointer p, size_type n){
+		size_t pages=((n*sizeof(value_type))/VMM_PAGE_SIZE)+1;
+		take_lock(sch_lock);
+		bool lock_vmm=true;
+		if(vmm_lock==sch_get_id()){
+			release_lock(vmm_lock);
+		}else{
+			lock_vmm=false;
+			release_lock(sch_lock);
+			take_lock(vmm_lock);
+			take_lock(sch_lock);
+			release_lock(vmm_lock);
+		}
+		vmm_free(p, pages);
+		if(lock_vmm) take_lock(vmm_lock);
+        release_lock(sch_lock);
+	}
+
+	size_t max_size() const throw(){
+		return (size_t)0xFFFFFFFF;
+	}
+	void construct ( pointer p, const_reference val ){
+		new ((void*)p) value_type (val);
+	}
+	void destroy (pointer p){
+		p->~T();
+	}
+	pointer address ( reference x ) const{
+		return &x;
+	}
+	const_pointer address ( const_reference x ) const{
+		return &x;
+	}
+};
+
+typedef map<uint32_t, amm_pagedetails, less<amm_pagedetails>, amm_allocator<pair<const uint32_t, amm_pagedetails> > > amm_alloc_map;
 amm_alloc_map *amm_allocated_pages;
 
 static bool amm_inited=false;
 static lock amm_lock;
-extern lock la_lock, vmm_lock, sch_lock;
 extern char _start, _end;
 static bool in_reserve=false;
 
@@ -31,25 +93,13 @@ void amm_mark_alloc(uint32_t pageaddr, amm_flags::Enum flags, pid_t owner, void 
 	if(!amm_inited) return;
 	amm_pagedetails p={flags, owner, ptr};
 	if(!in_reserve && amm_allocated_pages->capacity() < amm_allocated_pages->size() + 128){
-		//TODO: Make this "good" - need custom allocator for amm_allocated_pages that uses VMM directly.
-		take_lock(sch_lock);
-		release_lock(vmm_lock);
-		bool la=false;
-		if(la_lock==sch_get_id()){
-			release_lock(la_lock);
-			la=true;
-		}
 		in_reserve=true;
 		amm_allocated_pages->reserve(amm_allocated_pages->capacity() + 1024);
 		in_reserve=false;
-		if(la)take_lock(la_lock);
-		take_lock(vmm_lock);
-		release_lock(sch_lock);
 	}
 	{	hold_lock hl(amm_lock);
 		amm_allocated_pages->insert(amm_alloc_map::value_type(pageaddr, p));
 	}
-	//amm_allocated_pages->reserve(amm_allocated_pages->size() + 1);
 }
 
 void amm_mark_free(uint32_t pageaddr){
