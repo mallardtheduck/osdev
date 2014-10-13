01a1fe7d31cfdcf055d782b3df3367ee1e1e3c9a
stuartb <stuartbrockman@gmail.com>
2014-10-13 15:13:18 +0100

    termctl can also turn echo on/off.

--- src/user/termctl/termctl.c
+++ src/user/termctl/termctl.c
@@ -2,17 +2,28 @@
 #include <btos_stubs.h>
 #include <terminal.h>
 #include <string.h>
+#include <stdbool.h>
+
+bt_handle btos_get_handle(int fd);
 
 int main(int argc, char **argv){
-    if(argc < 2){
-        printf("Sets the terminal title.\n");
-        printf("Usage: %s title\n", argv[0]);
+    if(argc < 3){
+        iprintf("Change terminal configuration.\n");
+        iprintf("Usage: %s command [params...]\n", argv[0]);
         return 1;
     }
-    char path[BT_MAX_PATH]="";
-    if(!bt_getenv("STDOUT", path, BT_MAX_PATH)) return -1;
-    bt_filehandle fh= bt_fopen(path, FS_Write);
+    bt_filehandle fh=btos_get_handle(fileno(stdout));
     if(!fh) return -1;
-    bt_fioctl(fh, bt_terminal_ioctl_SetTitle, strlen(argv[1]), argv[1]);
+    if(strcmp(argv[1], "title")==0) {
+        bt_fioctl(fh, bt_terminal_ioctl_SetTitle, strlen(argv[2]), argv[2]);
+    }else if(strcmp(argv[1], "echo")) {
+        if(strcmp(argv[2], "on")==0) {
+            bool val = true;
+            bt_fioctl(fh, bt_terminal_ioctl_SetEcho, 1, (char*)&val);
+        }else if(strcmp(argv[2], "off")==0){
+            bool val = false;
+            bt_fioctl(fh, bt_terminal_ioctl_SetEcho, 1, (char*)&val);
+        }else return 1;
+    }
     return 0;
 }
