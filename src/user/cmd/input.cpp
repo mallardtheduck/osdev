f4e0581dbf769fd84973acabb8f55df985a185e2
stuartb <stuartbrockman@gmail.com>
2014-10-14 12:39:40 +0100

    Drop support for reading directly from keyboard device.

--- src/user/cmd/input.cpp
+++ src/user/cmd/input.cpp
@@ -13,7 +13,6 @@
 using namespace std;
 
 bt_filehandle input_fh=0;
-bool convert_input=false;
 bool input_tty=true;
 
 extern "C" bt_handle btos_get_handle(int fd);
@@ -21,69 +20,30 @@ extern "C" bt_handle btos_get_handle(int fd);
 void open_input(){
     bt_filehandle fh=btos_get_handle(fileno(stdin));
 	if(!fh) {
-        string device="DEV:/" + get_env("INPUT_DEVICE");
-		convert_input=true;
-        fh=bt_fopen(device.c_str(), FS_Read);
-        if(!fh){
-            cerr << "Error: Could not open input file/device: " << device.c_str() << endl;
-            exit(-1);
-        }
+        cerr << "Error: Could not open input file/device: " << get_env("STDIN") << endl;
+        exit(-1);
 	}
 	input_fh=fh;
     size_t type=bt_fioctl(input_fh, bt_ioctl::DevType, 0, NULL);
     if(type!=driver_types::TERMINAL && (type & driver_types::INPUT)!=driver_types::INPUT) input_tty=false;
 }
 
-char get_char(){
-	if(!input_fh) open_input();
-	if(convert_input){
-		uint32_t val;
-		while(bt_fread(input_fh, sizeof(val), (char*)&val)){
-			if(KB_char(val)){
-				return val;
-			}
-		}
-		return '\0';
-	}else{
-		char ret='\0';
-		size_t bytes=bt_fread(input_fh, sizeof(ret), &ret);
-        if(!input_tty && bytes==0){
-            cerr << "Warning: End of input file/device reached." << endl;
-            exit(0);
-        }
-		return ret;
-	}
+char get_char() {
+    if (!input_fh) open_input();
+    char ret = '\0';
+    size_t bytes = bt_fread(input_fh, sizeof(ret), &ret);
+    if (!input_tty && bytes == 0) {
+        cerr << "Warning: End of input file/device reached." << endl;
+        exit(0);
+    }
+    return ret;
+
 }
 
 string input_line(){
-    if(convert_input) {
-        stringstream ret;
-        if (!input_fh) open_input();
-        streampos pos;
-        if (input_tty) pos = cout.tellp();
-        while (char c = get_char()) {
-            if (c == '\n') break;
-            else if (c == 0x08) {
-                string data = ret.str();
-                if (data.length())data.erase(data.length() - 1);
-                ret.str(data);
-                ret.seekp(0, ios::end);
-            } else ret << c;
-            if (input_tty) {
-                cout.seekp(pos);
-                cout << ret.str();
-                streampos newpos = cout.tellp();
-                cout << ' ';
-                cout.seekp(newpos);
-            }
-        }
-        if (input_tty) cout << endl;
-        return ret.str();
-    }else {
-        string ret;
-        getline(cin, ret);
-        return ret;
-    }
+    string ret;
+    getline(cin, ret);
+    return ret;
 }
 
 vector<string> parse_input(const string &input){
