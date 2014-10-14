c50afacf2f5a4513e60e61504f2fb801ffd13eec
stuartb <stuartbrockman@gmail.com>
2014-10-14 09:25:44 +0100

    Fixed "echo" conditional and added "clear" command to termctl.

--- src/user/termctl/termctl.c
+++ src/user/termctl/termctl.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <btos_stubs.h>
 #include <terminal.h>
+#include <video_dev.h>
 #include <string.h>
 #include <stdbool.h>
 
 bt_handle btos_get_handle(int fd);
 
 int main(int argc, char **argv){
-    if(argc < 3){
+    if(argc < 2 && argv[1]){
         iprintf("Change terminal configuration.\n");
         iprintf("Usage: %s command [params...]\n", argv[0]);
         return 1;
@@ -16,7 +17,7 @@ int main(int argc, char **argv){
     if(!fh) return -1;
     if(strcmp(argv[1], "title")==0) {
         bt_fioctl(fh, bt_terminal_ioctl_SetTitle, strlen(argv[2]), argv[2]);
-    }else if(strcmp(argv[1], "echo")) {
+    }else if(strcmp(argv[1], "echo")==0 && argc == 3) {
         if(strcmp(argv[2], "on")==0) {
             bool val = true;
             bt_fioctl(fh, bt_terminal_ioctl_SetEcho, 1, (char*)&val);
@@ -24,6 +25,8 @@ int main(int argc, char **argv){
             bool val = false;
             bt_fioctl(fh, bt_terminal_ioctl_SetEcho, 1, (char*)&val);
         }else return 1;
+    }else if(strcmp(argv[1], "clear")==0) {
+        bt_fioctl(fh, bt_vid_ioctl_ClearScreen, 0, NULL);
     }
     return 0;
 }
