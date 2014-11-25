9075300156de3f51cf1c3618bb8744bd6516a88b
mallard <stuartbrockman@gmail.com>
2014-11-25 21:45:45 +0000

    Tidied up disk and initfs makefiles, removed unneccisary files from initfs.

--- src/kernel/initfs/initfs_data.cpp
+++ src/kernel/initfs/initfs_data.cpp
@@ -6,10 +6,8 @@ unsigned char null_file[] = {};
 initfs_file initfs_data[] = {
 	{true, "NULL.FILE", null_file, sizeof(null_file)},
 	{true, "CONFIG.INI", __data_config_ini, sizeof(__data_config_ini)},
-	{true, "TEST.SYS", __data_test_sys, sizeof(__data_test_sys)},
 	{true, "BOOT.SYS", __data_boot_sys, sizeof(__data_boot_sys)},
 	{true, "RTC.SYS", __data_rtc_sys, sizeof(__data_rtc_sys)},
-	{true, "TEST.ELX", __data_test_elx, sizeof(__data_test_elx)},
 	{true, "KEYBOARD.SYS", __data_keyboard_sys, sizeof(__data_keyboard_sys)},
 	{true, "ATA.SYS", __data_ata_sys, sizeof(__data_ata_sys)},
 	{true, "FAT.SYS", __data_fat_sys, sizeof(__data_fat_sys)},
