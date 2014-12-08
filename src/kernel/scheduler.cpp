cc5c39a207ddb89c361f7ff32c46b7de1cdcc6f8
stuartb <stuartbrockman@gmail.com>
2014-12-08 11:44:50 +0000

    Re-designed scheduler to use separate "prescheduler" thread, so that main scheduler is simplified.

--- src/kernel/scheduler.cpp
+++ src/kernel/scheduler.cpp
@@ -36,19 +36,24 @@ struct sch_thread{
 	bool abortable;
 	int abortlevel;
     bool user_abort;
+	sch_thread *next;
+	uint32_t sch_cycle;
 };
 
-vector<sch_thread> *threads;
-size_t current_thread;
+vector<sch_thread*> *threads;
+sch_thread *current_thread;
 uint64_t current_thread_id;
-size_t reaper_thread;
+sch_thread *reaper_thread;
+sch_thread *prescheduler_thread;
 uint64_t cur_ext_id;
-size_t idle_thread;
+sch_thread *idle_thread;
 uint64_t sch_zero=0;
 
 void thread_reaper(void*);
 void sch_threadtest();
 void sch_idlethread(void*);
+void sch_prescheduler_thread(void*);
+static sch_thread *sch_get(uint64_t ext_id);
 
 lock sch_lock;
 bool sch_inited=false;
@@ -61,7 +66,7 @@ char *sch_threads_infofs(){
 	sprintf(buffer, "# ID, PID, priority, addr, run, alevel\n");
 	{hold_lock hl(sch_lock);
 		for(size_t i=0; i<threads->size(); ++i){
-			sch_thread *t=&(*threads)[i];
+			sch_thread *t=(*threads)[i];
 			sprintf(&buffer[strlen(buffer)],"%i, %i, %i, %x, %i, %i\n", (int)t->ext_id, (int)t->pid, t->priority, t->eip,
 				(bool)t->runnable, t->abortlevel);
 		}
@@ -80,31 +85,31 @@ void sch_init(){
 	outb(0x43, 0x36);
 	outb(0x40, value & 0xFF);
 	outb(0x40, (value >> 8) & 0xFF);
-	threads=new vector<sch_thread>();
+	threads=new vector<sch_thread*>();
 	sch_stack=(char*)malloc(4096)+4096;
-	sch_thread mainthread;
-	mainthread.runnable=true;
-	mainthread.to_be_deleted=false;
-	mainthread.priority=default_priority;
-	mainthread.dynpriority=0;
-	mainthread.magic=0xF00D;
-	mainthread.pid=proc_current_pid;
-	mainthread.blockcheck=NULL;
-	mainthread.bc_param=NULL;
-	mainthread.abortlevel=1;
-	mainthread.abortable=false;
-	mainthread.pid=0;
-	current_thread_id=mainthread.ext_id=++cur_ext_id;
+	sch_thread *mainthread=new sch_thread();
+	mainthread->runnable=true;
+	mainthread->to_be_deleted=false;
+	mainthread->priority=default_priority;
+	mainthread->dynpriority=0;
+	mainthread->magic=0xF00D;
+	mainthread->pid=proc_current_pid;
+	mainthread->blockcheck=NULL;
+	mainthread->bc_param=NULL;
+	mainthread->abortlevel=1;
+	mainthread->abortable=false;
+	mainthread->pid=0;
+	mainthread->sch_cycle=0;
+	current_thread_id=mainthread->ext_id=++cur_ext_id;
 	threads->push_back(mainthread);
-	current_thread=threads->size()-1;
-	sch_new_thread(&sch_idlethread, NULL, 1024);
-	reaper_thread=sch_new_thread(&thread_reaper, NULL, 4096);
-	for(size_t i=0; i<(*threads).size(); ++i){
-		if((*threads)[i].ext_id==reaper_thread){
-			reaper_thread=i;
-			break;
-		}
-	}
+	current_thread=(*threads)[threads->size()-1];
+	uint64_t idle_thread_id=sch_new_thread(&sch_idlethread, NULL, 1024);
+	idle_thread=sch_get(idle_thread_id);
+	uint64_t reaper_thread_id=sch_new_thread(&thread_reaper, NULL, 4096);
+	reaper_thread=sch_get(reaper_thread_id);
+	uint64_t prescheduler_id=sch_new_thread(&sch_prescheduler_thread, NULL, 1024);
+	prescheduler_thread=sch_get(prescheduler_id);
+	current_thread->next=prescheduler_thread;
 	//sch_threadtest();
 	irq_handle(0, &sch_isr);
 	sch_inited=true;
@@ -117,7 +122,7 @@ void test_priority(void *params){
 	uint32_t *p=(uint32_t*)params;
 	char c=(char)(p[0]);
 	uint32_t priority=p[1];
-	(*threads)[current_thread].priority=priority;
+	current_thread->priority=priority;
 	free(params);
 	while(true){
 		printf("%c", c);
@@ -137,7 +142,6 @@ void sch_threadtest(){
 }
 
 void sch_idlethread(void*){
-	idle_thread=current_thread_id;
 	sch_set_priority(0xFFFFFFFF);
 	while(true)asm volatile("hlt");
 }
@@ -147,10 +151,10 @@ extern "C" void sch_wrapper(){
     sch_start *start;
     {
         hold_lock hl(sch_lock);
-        start = (*threads)[current_thread].start;
-        ext_id=(int)(*threads)[current_thread].ext_id;
+        start = current_thread->start;
+        ext_id=(int)current_thread->ext_id;
     }
-	dbgpf("SCH: Starting new thread %i (%i) at %x (param %x) [%x].\n", current_thread, ext_id, start->ptr, start->param, start);
+	dbgpf("SCH: Starting new thread %x (%i) at %x (param %x) [%x].\n", current_thread, (int)ext_id, start->ptr, start->param, start);
     void (*entry)(void*) = start->ptr;
     void *param = start->param;
     free(start);
@@ -159,35 +163,37 @@ extern "C" void sch_wrapper(){
 }
 
 uint64_t sch_new_thread(void (*ptr)(void*), void *param, size_t stack_size){
-	sch_thread newthread;
+	sch_thread *newthread=new sch_thread();
 	sch_start *start=(sch_start*)malloc(sizeof(sch_start));
 	start->ptr=ptr;
 	start->param=param;
 	uint32_t stack=(uint32_t)malloc(stack_size);
-	newthread.stackptr=(void*)stack;
+	newthread->stackptr=(void*)stack;
 	stack+=stack_size;
 	stack-=4;
 	*(uint32_t*)stack=(uint32_t)&sch_wrapper;
-	newthread.stackbase=(void*)stack;
-	newthread.stack.ss=0x10;
-	newthread.stack.esp=stack;
-	newthread.start=start;
-	newthread.runnable=true;
-	newthread.to_be_deleted=false;
-	newthread.magic=0xBABE;
-	newthread.priority=default_priority;
-	newthread.dynpriority=0;
-	newthread.blockcheck=NULL;
-	newthread.bc_param=NULL;
-	newthread.abortlevel=1;
-	newthread.abortable=false;
-	newthread.pid=0;
-    newthread.user_abort=false;
+	newthread->stackbase=(void*)stack;
+	newthread->stack.ss=0x10;
+	newthread->stack.esp=stack;
+	newthread->start=start;
+	newthread->runnable=true;
+	newthread->to_be_deleted=false;
+	newthread->magic=0xBABE;
+	newthread->priority=default_priority;
+	newthread->dynpriority=0;
+	newthread->blockcheck=NULL;
+	newthread->bc_param=NULL;
+	newthread->abortlevel=1;
+	newthread->abortable=false;
+	newthread->pid=0;
+    newthread->user_abort=false;
+	newthread->next=NULL;
+	newthread->sch_cycle=0;
     take_lock_exclusive(sch_lock);
-	newthread.ext_id=++cur_ext_id;
+	newthread->ext_id=++cur_ext_id;
 	threads->push_back(newthread);
 	release_lock(sch_lock);
-	return newthread.ext_id;
+	return newthread->ext_id;
 }
 
 void thread_reaper(void*){
@@ -198,12 +204,14 @@ void thread_reaper(void*){
 			hold_lock lck(sch_lock);
 			changed=false;
 			for(size_t i=0; i<threads->size(); ++i){
-				if((*threads)[i].to_be_deleted){
-					uint64_t id=(*threads)[i].ext_id;
-                    void *stackptr=(*threads)[i].stackptr;
+				if((*threads)[i]->to_be_deleted){
+					sch_thread *ptr=(*threads)[i];
+					uint64_t id=(*threads)[i]->ext_id;
+                    void *stackptr=(*threads)[i]->stackptr;
 					threads->erase(i);
                     release_lock(sch_lock);
                     free(stackptr);
+					delete ptr;
                     take_lock_exclusive(sch_lock);
 					changed=true;
 					dbgpf("SCH: Reaped %i (%i) [%x].\n", i, (uint32_t)id, stackptr);
@@ -216,11 +224,11 @@ void thread_reaper(void*){
 }
 
 void sch_end_thread(){
-    proc_remove_thread(sch_get_id(), (*threads)[current_thread].pid);
+    proc_remove_thread(sch_get_id(), current_thread->pid);
     take_lock_exclusive(sch_lock);
-	(*threads)[current_thread].runnable=false;
-	(*threads)[current_thread].to_be_deleted=true;
-	(*threads)[reaper_thread].runnable=true;
+	current_thread->runnable=false;
+	current_thread->to_be_deleted=true;
+	reaper_thread->runnable=true;
 	release_lock(sch_lock);
 	sch_yield();
 	panic("SCH: Attempt to run to_be_deleted thread!");
@@ -234,20 +242,21 @@ inline void out_regs(const irq_regs &ctx){
 	dbgpf("EFLAGS: %x ORESP: %x\n", ctx.eflags, ctx.useresp);
 }
 
-bool sch_find_thread(size_t &torun){
+bool sch_find_thread(sch_thread *&torun){
 	//Reset dyanmic priority of idle thread (ensures it only runs when nothing else is available)
-	(*threads)[idle_thread].dynpriority=(*threads)[idle_thread].priority;
+	//(*threads)[idle_thread].dynpriority=(*threads)[idle_thread].priority;
 	//Find runnable threads and minimum dynamic priority
 	int nrunnables=0;
 	uint32_t min=0xFFFFFFFF;
 	for(size_t i=0; i<threads->size(); ++i){
-	    if(!(*threads)[i].runnable && (*threads)[i].blockcheck!=NULL){
-	        (*threads)[i].runnable=(*threads)[i].blockcheck((*threads)[i].bc_param);
+		if(!(*threads)[i]->priority==0xFFFFFFFF) (*threads)[i]->dynpriority=0xFFFFFFFF;
+	    if(!(*threads)[i]->runnable && (*threads)[i]->blockcheck!=NULL){
+	        (*threads)[i]->runnable=(*threads)[i]->blockcheck((*threads)[i]->bc_param);
 	    }
-		if((*threads)[i].runnable){
-			if(!(*threads)[i].priority) panic("(SCH) Thread priority 0 is not allowed.\n");
+		if((*threads)[i]->runnable){
+			if(!(*threads)[i]->priority) panic("(SCH) Thread priority 0 is not allowed.\n");
 			nrunnables++;
-			if((*threads)[i].dynpriority < min) min=(*threads)[i].dynpriority;
+			if((*threads)[i]->dynpriority < min) min=(*threads)[i]->dynpriority;
 		}
 	}
 
@@ -260,11 +269,11 @@ bool sch_find_thread(size_t &torun){
 	//that isn't the current thread, record it
 	bool foundtorun=false;
 	for(size_t i=0; i<(*threads).size(); ++i){
-		if((*threads)[i].runnable){
-			if((*threads)[i].dynpriority) (*threads)[i].dynpriority-=min;
-			if(i!=current_thread && (*threads)[i].dynpriority==0){
+		if((*threads)[i]->runnable){
+			if((*threads)[i]->dynpriority) (*threads)[i]->dynpriority-=min;
+			if((*threads)[i]!=current_thread && (*threads)[i]->dynpriority==0){
 				foundtorun=true;
-				torun=i;
+				torun=(*threads)[i];
 			}
 		}
 	}
@@ -281,26 +290,25 @@ extern "C" sch_stackinfo *sch_schedule(uint32_t ss, uint32_t esp){
 	if(get_lock_owner(sch_lock)!=current_thread_id) panic("(SCH) Bad scheduler locking detected!\n");
 	
 	//Save current thread's state
-	(*threads)[current_thread].stack.ss=ss;
-	(*threads)[current_thread].stack.esp=esp;
+	current_thread->stack.ss=ss;
+	current_thread->stack.esp=esp;
 
-	size_t torun;
-	//If we found a thread to run, run it
-	if(sch_find_thread(torun)){
-		(*threads)[torun].dynpriority=(*threads)[torun].priority;
-		current_thread=torun;
-		curstack=(*threads)[current_thread].stack;
-        if(!(*threads)[torun].ext_id) panic("(SCH) Thread with no ID?");
-        lock_transfer(sch_lock, (*threads)[torun].ext_id);
-        current_thread_id=(*threads)[torun].ext_id;
-		proc_switch_sch((*threads)[current_thread].pid, false);
-		gdt_set_kernel_stack((*threads)[current_thread].stackbase);
-		return &curstack;		
-	}else{
-		//Nothing to run?
-		panic("(SCH) No runnable threads (idle thread not runnable?)!\n:");
-	}
-	release_lock(sch_lock);
+	//Get next thread
+	sch_thread *torun=current_thread->next;
+
+	//Clear old thread's next value, to prevent accidents
+	current_thread->next=NULL;
+	//If the thread exists, but isn't runnable (made non-runnable since last preschedule), skip it
+	if(torun && !torun->runnable) torun=torun->next;
+	//If there is no next, run the prescheduler instead
+	if(!torun) torun=prescheduler_thread;
+	current_thread=torun;
+	curstack=current_thread->stack;
+	if(!torun->ext_id) panic("(SCH) Thread with no ID?");
+	lock_transfer(sch_lock, torun->ext_id);
+	current_thread_id=torun->ext_id;
+	proc_switch_sch(current_thread->pid, false);
+	gdt_set_kernel_stack(current_thread->stackbase);
 	return &curstack;
 }
 
@@ -324,7 +332,7 @@ void sch_isr(int irq, isr_regs *regs){
         if(!counter) {
             counter=cstart;
             sch_abortable(true);
-            (*threads)[current_thread].eip = regs->eip;
+			current_thread->eip = regs->eip;
             release_lock(sch_lock);
             enable_interrupts();
             sch_yield();
@@ -343,12 +351,12 @@ const uint64_t &sch_get_id(){
 
 void sch_set_priority(uint32_t pri){
 	hold_lock hl(sch_lock);
-	(*threads)[current_thread].priority=pri;
+	current_thread->priority=pri;
 }
 
 void sch_block(){
     take_lock_recursive(sch_lock);
-	(*threads)[current_thread].runnable=false;
+	current_thread->runnable=false;
 	release_lock(sch_lock);
 	sch_yield();
 }
@@ -356,8 +364,8 @@ void sch_block(){
 void sch_unblock(uint64_t ext_id){
 	hold_lock hl(sch_lock);
 	for(size_t i=0; i<threads->size(); ++i){
-		if((*threads)[i].ext_id==ext_id && !(*threads)[i].to_be_deleted){
-			(*threads)[i].runnable=true;
+		if((*threads)[i]->ext_id==ext_id && !(*threads)[i]->to_be_deleted){
+			(*threads)[i]->runnable=true;
 			break;
 		}
 	}
@@ -369,13 +377,13 @@ bool sch_active(){
 
 void sch_setpid(pid_t pid){
 	hold_lock hl(sch_lock, false);
-	(*threads)[current_thread].pid=pid;
+	current_thread->pid=pid;
 }
 
 void sch_setblock(sch_blockcheck check, void *param){
     { hold_lock hl(sch_lock);
-        (*threads)[current_thread].blockcheck=check;
-        (*threads)[current_thread].bc_param=param;
+		current_thread->blockcheck=check;
+		current_thread->bc_param=param;
     }
     sch_abortable(true);
     sch_block();
@@ -385,14 +393,14 @@ void sch_setblock(sch_blockcheck check, void *param){
 
 void sch_clearblock(){
 	hold_lock hl(sch_lock, false);
-	(*threads)[current_thread].blockcheck=NULL;
-	(*threads)[current_thread].bc_param=NULL;
+	current_thread->blockcheck=NULL;
+	current_thread->bc_param=NULL;
 }
 
 bool sch_wait_blockcheck(void *p){
 	uint64_t &ext_id=*(uint64_t*)p;
 	for(size_t i=0; i<threads->size(); ++i){
-		if((*threads)[i].ext_id==ext_id){
+		if((*threads)[i]->ext_id==ext_id){
 			return false;
 		}
 	}
@@ -402,7 +410,7 @@ bool sch_wait_blockcheck(void *p){
 void sch_wait(uint64_t ext_id){
     take_lock_exclusive(sch_lock);
 	for(size_t i=0; i<threads->size(); ++i){
-		if((*threads)[i].ext_id==ext_id){
+		if((*threads)[i]->ext_id==ext_id){
             release_lock(sch_lock);
 			sch_setblock(sch_wait_blockcheck, (void*)&ext_id);
             take_lock_exclusive(sch_lock);
@@ -414,12 +422,12 @@ void sch_wait(uint64_t ext_id){
 
 extern "C" void sch_update_eip(uint32_t eip){
     hold_lock hl(sch_lock, false);
-	(*threads)[current_thread].eip=eip;
+	current_thread->eip=eip;
 }
 
 uint32_t sch_get_eip(bool lock){
     if(lock) take_lock_recursive(sch_lock);
-    uint32_t ret=(*threads)[current_thread].eip;
+    uint32_t ret=current_thread->eip;
     if(lock) release_lock(sch_lock);
     return ret;
 }
@@ -428,24 +436,24 @@ void sch_abortable(bool abortable){
     int alevel;
     {
         hold_lock hl(sch_lock, false);
-        (*threads)[current_thread].abortlevel += abortable ? -1 : 1;
-        alevel = (*threads)[current_thread].abortlevel;
+		current_thread->abortlevel += abortable ? -1 : 1;
+        alevel = current_thread->abortlevel;
     }
 	if(alevel<=0){
         hold_lock hl(sch_lock, false);
-		(*threads)[current_thread].abortlevel=0;
-		(*threads)[current_thread].abortable=true;
+		current_thread->abortlevel=0;
+		current_thread->abortable=true;
 	}else{
         hold_lock hl(sch_lock, false);
-		(*threads)[current_thread].abortable=false;
+		current_thread->abortable=false;
 	}
 }
 
 bool sch_abort_blockcheck(void *p){
 	uint64_t &ext_id=*(uint64_t*)p;
 	for(size_t i=0; i<threads->size(); ++i){
-		if((*threads)[i].ext_id==ext_id){
-			return (*threads)[i].abortable;
+		if((*threads)[i]->ext_id==ext_id){
+			return (*threads)[i]->abortable;
 		}
 	}
 	return true;
@@ -457,15 +465,15 @@ void sch_abort(uint64_t ext_id){
         take_lock_recursive(sch_lock);
 		bool found=false;
 		for(size_t i=0; i<threads->size(); ++i){
-			if((*threads)[i].ext_id==ext_id){
+			if((*threads)[i]->ext_id==ext_id){
 				found=true;
-				if((*threads)[i].abortable){
-					(*threads)[i].runnable=false;
-					(*threads)[i].to_be_deleted=true;
-					(*threads)[reaper_thread].runnable=true;
+				if((*threads)[i]->abortable){
+					(*threads)[i]->runnable=false;
+					(*threads)[i]->to_be_deleted=true;
+					reaper_thread->runnable=true;
 					tryagain=false;
 				}else{
-                    (*threads)[i].user_abort=true;
+                    (*threads)[i]->user_abort=true;
                 }
 			}
 		}
@@ -485,7 +493,37 @@ bool sch_can_lock(){
 
 bool sch_user_abort(){
     take_lock_recursive(sch_lock);
-    bool ret=(*threads)[current_thread].user_abort;
+    bool ret=current_thread->user_abort;
     release_lock(sch_lock);
     return ret;
+}
+
+void sch_prescheduler_thread(void*){
+	current_thread->runnable=false;
+	uint32_t cycle=0;
+	while(true){
+		cycle++;
+		take_lock_exclusive(sch_lock);
+		sch_thread *current=current_thread;
+		sch_thread *next=NULL;
+		while(sch_find_thread(next)){
+			if(next->sch_cycle==cycle){
+				break;
+			}
+			current->next=next;
+			current=current->next;
+			current->dynpriority=current->priority;
+			current->sch_cycle=cycle;
+		}
+		release_lock(sch_lock);
+		sch_yield();
+	}
+}
+
+static sch_thread *sch_get(uint64_t ext_id){
+	hold_lock hl(sch_lock);
+	for(size_t i=0; i<threads->size(); ++i){
+		if((*threads)[i]->ext_id==ext_id) return (*threads)[i];
+	}
+	return NULL;
 }
