47da3cd775fd2b8942f8e3f92066bdaa9b8875e6
mallard <stuartbrockman@gmail.com>
2014-12-29 23:07:41 +0000

    Added mouse test program.

---
+++ src/user/mousetest/mousetest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <mouse.h>
+#include <btos_stubs.h>
+
+int main(){
+    bt_filehandle fh= bt_fopen("DEV:/MOUSE0", FS_Read);
+    while(1){
+        bt_mouse_packet packet;
+        bt_fread(fh, sizeof(bt_mouse_packet), (char*)&packet);
+        printf("Flags: %x, X: %i, Y: %i\n", packet.flags, packet.x_motion, packet.y_motion);
+    }
+    return 0;
+}
