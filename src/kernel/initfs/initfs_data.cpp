77ca5f9f5b957c1e31c4faf47fe78f9c60fee770
mallard <stuartbrockman@gmail.com>
2014-12-29 10:57:38 +0000

    Renamed keyboard.sys to ps2.sys.

--- src/kernel/initfs/initfs_data.cpp
+++ src/kernel/initfs/initfs_data.cpp
@@ -8,7 +8,7 @@ initfs_file initfs_data[] = {
 	{true, "CONFIG.INI", __data_config_ini, sizeof(__data_config_ini)},
 	{true, "BOOT.SYS", __data_boot_sys, sizeof(__data_boot_sys)},
 	{true, "RTC.SYS", __data_rtc_sys, sizeof(__data_rtc_sys)},
-	{true, "KEYBOARD.SYS", __data_keyboard_sys, sizeof(__data_keyboard_sys)},
+	{true, "PS2.SYS", __data_ps2_sys, sizeof(__data_ps2_sys)},
 	{true, "ATA.SYS", __data_ata_sys, sizeof(__data_ata_sys)},
 	{true, "FAT.SYS", __data_fat_sys, sizeof(__data_fat_sys)},
     {true, "SPAWN.ELX", __data_spawn_elx, sizeof(__data_spawn_elx)},
