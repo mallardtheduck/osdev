7cd3b1e0d30b8bd7ba38dc2990180d7f9c5433b8
Stuart Brockman <stuartbrockman@gmail.com>
2015-11-16 16:01:34 +0000

    Apply bounds check to width and height.

--- src/user/gds/screen.cpp
+++ src/user/gds/screen.cpp
@@ -129,8 +129,8 @@ bool Screen::SetMode(uint32_t w, uint32_t h, uint8_t bpp) {
 void Screen::UpdateScreen(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
 	bool hide_cursor = false;	
 	if (!x && !y && !w && !h) {
-		w = current_mode.width;
-		h = current_mode.height;
+		w = current_mode.width - 1;
+		h = current_mode.height - 1;
 	}
 	if(cursor_on){
 		hide_cursor = true;
@@ -151,6 +151,8 @@ void Screen::UpdateScreen(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
 	}
 	if(y > current_mode.height) return;
 	if(x > current_mode.width) return;
+	if(x + w >= current_mode.width) w = current_mode.width - x - 1;
+	if(y + h >= current_mode.height) h = current_mode.height - y - 1;
 	for(uint32_t row=y; row <= y+h; ++row){
 		if(row > current_mode.height) break;
 		for(uint32_t col=x; col <= x+w; ++col){
@@ -173,7 +175,7 @@ void Screen::UpdateScreen(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
 		for(size_t row=y; row <= y+h; ++row){
 			size_t start= GetBytePos(x, row);
 			if(start > buffersize) break;
-			size_t end= GetBytePos(x+w, row) + BytesPerPixel() + 1;
+			size_t end= GetBytePos(x+w, row) + BytesPerPixel() + 2;
 			if(end > buffersize) end=buffersize;
 			size_t bytes = end - start;
 			bt_fseek(fh, start, false);
