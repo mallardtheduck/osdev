3ece66da143ea5c14d5b8b3ec48bc538d6820473
Stuart Brockman <stuartbrockman@gmail.com>
2016-02-01 12:09:23 +0000

    Added support for dirent.h functions. Also recovered "lost" getenv implementation.

---
+++ src/toolchain/newlib-2.1.0/newlib/libc/sys/btos/dirent.c
@@ -0,0 +1,38 @@
+#include "dirent.h"
+#include <stdlib.h>
+#include <string.h>
+
+DIR *opendir (const char *path){
+	DIR *ret = malloc(sizeof(DIR));
+	if(ret) *ret = bt_dopen(path, FS_Read);
+	return ret;
+}
+
+int readdir_r (DIR *__restrict dirp, struct dirent *__restrict entry, struct dirent **__restrict result){
+	bt_directory_entry de = bt_dread(*dirp);
+	if(de.valid){
+		entry->d_ino = 0;
+		strncpy(entry->d_name, de.filename, MAXNAMLEN);
+		entry->d_namlen = strlen(entry->d_name);
+		entry->d_reclen = sizeof(entry);
+		if(result) *result = entry;
+		return 0;
+	}else{
+		if(result) *result = NULL;
+		return -1;
+	}
+}
+
+struct dirent *readdir (DIR *dirp){
+	static struct dirent ret;
+	readdir_r(dirp, &ret, NULL);
+}
+
+void rewinddir (DIR *dirp){
+	bt_dseek(*dirp, 0, 0);
+}
+
+int closedir (DIR *dirp){
+	bt_dclose(*dirp);
+	free(dirp);
+}
