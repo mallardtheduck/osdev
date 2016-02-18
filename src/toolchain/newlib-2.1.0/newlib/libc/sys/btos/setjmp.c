a4516a9156003d042287ad66cc8fc9c973bbe09f
Stuart Brockman <stuartbrockman@gmail.com>
2016-02-18 13:27:06 +0000

    Return 0 from non-implemented functions.

--- src/toolchain/newlib-2.1.0/newlib/libc/sys/btos/setjmp.c
+++ src/toolchain/newlib-2.1.0/newlib/libc/sys/btos/setjmp.c
@@ -13,10 +13,13 @@ void _longjmp(jmp_buf buf, int i) {
 
 int setjmp(jmp_buf buf) {
 	(void)buf;
+	return 0;
 }
 int sigsetjmp(sigjmp_buf buf, int i) {
 	(void)buf; (void)i;
+	return 0;
 }
 int _setjmp(jmp_buf buf) {
 	(void)buf;
+	return 0;
 }
