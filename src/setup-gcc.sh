2991e8f994c0e1e6ed918d7790324d822afc2a6b
Stuart Brockman <stuartbrockman@gmail.com>
2016-11-07 13:41:07 +0000

    Updated the ancient setup-binutils.sh and setup-gcc.sh to (re)build binutils and gcc for the i686-pc-btos target.

--- src/setup-gcc.sh
+++ src/setup-gcc.sh
@@ -1,16 +1,19 @@
 export PREFIX="$HOME/Projects/os/cross"
-export TARGET=i686-elf
+export TARGET=i686-pc-btos
 export PATH="$PREFIX/bin:$PATH"
 
-rm gcc-4.8.1.tar.bz2
 rm -rf gcc-4.8.1
-wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.1/gcc-4.8.1.tar.bz2
+if [ ! -f gcc-4.8.1.tar.bz2 ];
+then
+	wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.1/gcc-4.8.1.tar.bz2
+fi
 tar xvfj gcc-4.8.1.tar.bz2
 
 cd $HOME/Projects/os/src
 rm -rf build-gcc
 mkdir build-gcc
 cd build-gcc
+cp -Rv ../toolchain/gcc-4.8.1 ..
 ../gcc-4.8.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
 make all-target-libgcc
