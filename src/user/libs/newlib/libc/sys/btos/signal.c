f5aa837c891a035580784cde7810eff2498cffce
Stuart Brockman <stuartbrockman@gmail.com>
2017-01-10 13:37:28 +0000

    Added missing calls.

--- src/user/libs/newlib/libc/sys/btos/signal.c
+++ src/user/libs/newlib/libc/sys/btos/signal.c
@@ -1,7 +1,9 @@
 #include <signal.h>
 #include <sys/setjmp.h>
 
-int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) {}
-int sigprocmask(int how, const sigset_t *set, sigset_t *oset) {}
-_sig_func_ptr signal (int i, _sig_func_ptr p) {};
-int raise(int sig) {}
+int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) { return -1; }
+int sigprocmask(int how, const sigset_t *set, sigset_t *oset) { return -1; }
+_sig_func_ptr signal (int i, _sig_func_ptr p) { return SIG_ERR; };
+int raise(int sig) { return -1; }
+void siglongjmp(sigjmp_buf env, int val) {};
+int sigsetjmp(sigjmp_buf env, int savemask) { return 0; }
