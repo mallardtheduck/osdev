a7841364373ba9b4731f686f03aa1b9c37081963
Stuart Brockman <stuartbrockman@gmail.com>
2017-01-13 17:00:03 +0000

    Updated setup-gcc.sh.

--- src/setup-gcc.sh
+++ src/setup-gcc.sh
@@ -14,8 +14,9 @@ rm -rf build-gcc
 mkdir build-gcc
 cd build-gcc
 cp -Rv ../toolchain/gcc-4.8.1 ..
-../gcc-4.8.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --with-newlib --disable-multilib
+../gcc-4.8.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --with-newlib --disable-multilib --enable-shared=libgcc,libstdc++ --enable-initfini-array
 make all-gcc
-make all-target-libgcc
+SHLIB_LINK="i686-pc-btos-gcc -O2 -fPIC -shared @shlib_objs@ -o @shlib_base_name@.ell" make all-target-libgcc
 make install-gcc
 make install-target-libgcc
+find i686-pc-btos/libgcc -name \*.ell -exec cp {} ../../cross/i686-pc-btos/lib \;
