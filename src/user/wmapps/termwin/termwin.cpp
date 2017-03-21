a27c34fc553cf9461cdb5016f565241fbf76df41
Stuart Brockman <stuartbrockman@gmail.com>
2017-03-21 12:55:41 +0000

    Termwin now uses cached glyph images for faster rendering. Removed complex no-longer needed code for string-oriented draw optimsation.

--- src/user/wmapps/termwin/termwin.cpp
+++ src/user/wmapps/termwin/termwin.cpp
@@ -9,8 +9,10 @@
 #include <algorithm>
 #include <fstream>
 #include <cstring>
+#include <memory>
 #include "table.hpp"
 #include "dev/rtc.h"
+#include "lrucache.hpp"
 
 USE_BT_TERMINAL_API;
 
@@ -40,6 +42,16 @@ static volatile bool endrender = false;
 static size_t curpos;
 static uint64_t surf;
 static size_t lpos = SIZE_MAX;
+struct glyph_holder{
+	uint64_t gds_id;
+	glyph_holder(uint64_t id): gds_id(id) {}
+	~glyph_holder(){
+		GDS_SelectSurface(gds_id);
+		GDS_DeleteSurface();
+		GDS_SelectSurface(surf);
+	}
+};
+static cache::lru_cache<uint16_t, shared_ptr<glyph_holder>> glyphcache(1024);
 
 string get_env(const string &name){
 	char value[128];
@@ -104,28 +116,47 @@ void kill_children(){
 	}
 }
 
-uint32_t getcolour(uint8_t id){
+uint32_t getcolour(uint8_t id, bool cache = true){
 	static uint32_t colours[16] = {0};
-	if(id < 16 && colours[id]) return colours[id];
+	if(cache && id < 16 && colours[id]) return colours[id] - 1;
+	uint32_t ret;
 	switch(id){
-		case 0: return colours[0] = GDS_GetColour(0, 0, 0);
-		case 1: return colours[1] = GDS_GetColour(0, 0, 0xaa);
-		case 2: return colours[2] = GDS_GetColour(0, 0xaa, 0);
-		case 3: return colours[3] = GDS_GetColour(0, 0xaa, 0xaa);
-		case 4: return colours[4] = GDS_GetColour(0xaa, 0, 0);
-		case 5: return colours[5] = GDS_GetColour(0xaa, 0, 0xaa);
-		case 6: return colours[6] = GDS_GetColour(0xaa, 0x55, 0);
-		case 7: return colours[7] = GDS_GetColour(0xaa, 0xaa, 0xaa);
-		case 8: return colours[8] = GDS_GetColour(0x55, 0x55, 0x55);
-		case 9: return colours[9] = GDS_GetColour(0x55, 0x55, 0xff);
-		case 10: return colours[10] = GDS_GetColour(0x55, 0xff, 0x55);
-		case 11: return colours[11] = GDS_GetColour(0x55, 0xff, 0xff);
-		case 12: return colours[12] = GDS_GetColour(0xff, 0x55, 0x55);
-		case 13: return colours[13] = GDS_GetColour(0xff, 0x55, 0xff);
-		case 14: return colours[14] = GDS_GetColour(0xff, 0xff, 0x55);
-		case 15: return colours[15] = GDS_GetColour(0xff, 0xff, 0xff);
-		default: return colours[0] = GDS_GetColour(0, 0, 0);
+		case 0: ret = GDS_GetColour(0, 0, 0); break;
+		case 1: ret = GDS_GetColour(0, 0, 0xaa); break;
+		case 2: ret = GDS_GetColour(0, 0xaa, 0); break;
+		case 3: ret = GDS_GetColour(0, 0xaa, 0xaa); break;
+		case 4: ret = GDS_GetColour(0xaa, 0, 0); break;
+		case 5: ret = GDS_GetColour(0xaa, 0, 0xaa); break;
+		case 6: ret = GDS_GetColour(0xaa, 0x55, 0); break;
+		case 7: ret = GDS_GetColour(0xaa, 0xaa, 0xaa); break;
+		case 8: ret = GDS_GetColour(0x55, 0x55, 0x55); break;
+		case 9: ret = GDS_GetColour(0x55, 0x55, 0xff); break;
+		case 10: ret = GDS_GetColour(0x55, 0xff, 0x55); break;
+		case 11: ret = GDS_GetColour(0x55, 0xff, 0xff); break;
+		case 12: ret = GDS_GetColour(0xff, 0x55, 0x55); break;
+		case 13: ret = GDS_GetColour(0xff, 0x55, 0xff); break;
+		case 14: ret = GDS_GetColour(0xff, 0xff, 0x55); break;
+		case 15: ret = GDS_GetColour(0xff, 0xff, 0xff); break;
+		default: ret = GDS_GetColour(0, 0, 0); break;
 	}
+	if(cache && id < 16) colours[id] = ret + 1;
+	return ret;
+}
+
+uint64_t get_glyph(uint8_t attr, uint8_t ch){
+	if(ch < 32) ch = ' ';
+	uint16_t key = (attr << 8) | ch;
+	if(glyphcache.exists(key)) return glyphcache.get(key)->gds_id;
+	uint64_t id = GDS_NewSurface(gds_SurfaceType::Bitmap, font_width, font_height);
+	uint8_t bgcol = attr >> 4;
+	uint8_t fgcol = attr & 0x0F;
+	uint32_t bg = getcolour(bgcol, false);
+	uint32_t fg = getcolour(fgcol, false);
+	GDS_Box(0, 0, font_width, font_height, bg, bg, 1, gds_LineStyle::Solid, gds_FillStyle::Filled);
+	GDS_TextChar(0, font_height - (font_height / 5), ch, font, fontSize, fg);
+	glyphcache.put(key, make_shared<glyph_holder>(id));
+	GDS_SelectSurface(surf);
+	return id;
 }
 
 bool compare_chars(char a, char b){
@@ -153,6 +184,25 @@ static size_t detect_line_scroll(size_t line){
 	return 0;
 }
 
+static void addRect(wm_Rect &a, const wm_Rect &b){
+	int32_t ax1 = a.x;
+	int32_t ay1 = a.y;
+	int32_t ax2 = a.x + a.w;
+	int32_t ay2 = a.y + a.h;
+	int32_t bx1 = b.x;
+	int32_t by1 = b.y;
+	int32_t bx2 = b.x + b.w;
+	int32_t by2 = b.y + b.h;
+	if(bx1 < ax1) ax1 = bx1;
+	if(by1 < ay1) ay1 = by1;
+	if(bx2 > ax2) ax2 = bx2;
+	if(by2 > ay2) ay2 = by2;
+	a.x = ax1;
+	a.y = ay1;
+	a.w = (ax2 - ax1);
+	a.h = (ay2 - ay1);	
+}
+
 void render_terminal_thread(){
 	if(!terminal_handle) return;
 	uint64_t render_counted = 0;
@@ -167,14 +217,19 @@ void render_terminal_thread(){
 			WM_SetTitle(title);
 			strncpy(ltitle, title, WM_TITLE_MAX);
 		}
+		wm_Rect updateRect = {0, 0, 0, 0};
+		uint64_t prepStart = bt_rtc_millis();
 		for(size_t line = 0; line < terminal_mode.height; ++line){
 			if(!has_line_changed(line)) continue;
 			if(size_t sline = detect_line_scroll(line)){
 				uint64_t start = bt_rtc_millis();
 				GDS_Blit(surf, 0, sline * font_height, terminal_mode.width * font_width, font_height, 0, line * font_height, terminal_mode.width * font_width, font_height);
-				WM_UpdateRect(0, (int32_t)(line * font_height), terminal_mode.width * font_width, font_height);
+				addRect(updateRect, {0, (int32_t)(line * font_height), (terminal_mode.width * font_width), font_height});
 				size_t l0addr = (line * terminal_mode.width) * 2;
 				size_t l1addr = (sline * terminal_mode.width) * 2;
+				stringstream qss;
+				qss << "TW: l0addr: " << l0addr << " l1addr " << l1addr << endl;
+				bt_zero(qss.str().c_str());
 				memcpy(&buffer[l0addr], &buffer[l1addr], terminal_mode.width * 2);
 				uint64_t end = bt_rtc_millis();
 				stringstream ss;
@@ -182,7 +237,7 @@ void render_terminal_thread(){
 				bt_zero(ss.str().c_str());
 			}else{
 				uint64_t start = bt_rtc_millis();
-				vector<pair<size_t, uint16_t>> line_changes;
+				size_t ch = 0;
 				for(size_t col = 0; col < terminal_mode.width; ++col){
 					size_t bufaddr = ((line * terminal_mode.width) + col) * 2;
 					if(!compare_chars(tempbuffer[bufaddr], buffer[bufaddr]) || tempbuffer[bufaddr + 1] != buffer[bufaddr + 1] || bufaddr == curpos || bufaddr == lpos){
@@ -192,99 +247,26 @@ void render_terminal_thread(){
 							lpos = curpos;
 						}
 						else buffer[bufaddr + 1] = tempbuffer[bufaddr + 1];
-						uint16_t change = (buffer[bufaddr] > 32 ? buffer[bufaddr] : ' ') | buffer[bufaddr + 1] << 8;
-						line_changes.push_back(make_pair(col, change));
+						uint64_t glyph = get_glyph(buffer[bufaddr + 1], buffer[bufaddr]);
+						GDS_Blit(glyph, 0, 0, font_width, font_height, col * font_width, line * font_height, font_width, font_height);
+						addRect(updateRect, {(int32_t)(col * font_width), (int32_t)(line * font_height), font_width, font_height});
+						++ch;
 					}
-				}
-				uint64_t stage1 = bt_rtc_millis();
-				if(line_changes.size()){
-					uint64_t s2start = bt_rtc_millis();
-					size_t firststart = UINT32_MAX;
-					size_t overallend = 0;
-					size_t start = UINT32_MAX;
-					size_t end = 0;
-					stringstream text;
-					uint8_t col = 0, ncol = 0;
-					for(auto change : line_changes){
-						bool draw = false;
-						if(start == UINT32_MAX) start = end = change.first;
-						if(firststart == UINT32_MAX) firststart = change.first;
-						overallend = change.first;
-						if(change.first == end){
-							++end;
-							char c = change.second & 0xFF;
-							uint8_t ccol = (change.second & 0xFF00) >> 8;
-							if(ccol == col){
-								text << c;
-							}else{
-								ncol = ccol;
-								draw = true;
-							}
-						}else{
-							draw = true;
-						}
-						if(draw){
-							uint64_t drawstart = bt_rtc_millis();
-							size_t dchars = 0;
-							if(text.str().length()){
-								uint8_t bgcol = col >> 4;
-								uint8_t fgcol = col & 0x0F;
-								uint32_t width = text.str().length() * font_width;
-								GDS_Box(start * font_width, line * font_height, width , font_height, getcolour(bgcol), getcolour(bgcol), 1, gds_LineStyle::Solid, gds_FillStyle::Filled);
-								string str = text.str();
-								for(size_t i = 0; i < str.length(); ++i){
-									if(str[i] <= 32) continue;
-									GDS_TextChar((start + i) * font_width, ((line + 1) * font_height) - (font_height / 5), str[i], font, fontSize, getcolour(fgcol));
-									++dchars;
-								}
-								//GDS_Text(start * font_width, (line * font_height) + 14, text.str().c_str(), font, 14, getcolour(fgcol));
-								//WM_UpdateRect(start * font_width, line * font_height, width, font_height);
-							}
-							text.str("");
-							text << (char)(change.second & 0xFF);
-							start = change.first;
-							end = change.first + 1;
-							col = ncol;
-							uint64_t drawend = bt_rtc_millis();
-							stringstream dss;
-							dss << "TW: Draw: " << drawend - drawstart << "ms (" << dchars << " chars)" << endl;
-							bt_zero(dss.str().c_str());
-						}
-					}
-					uint64_t s2s1 = bt_rtc_millis();
-					if(start != UINT32_MAX){
-						uint64_t drawstart = bt_rtc_millis();
-						size_t dchars = 0;
-						uint8_t bgcol = col >> 4;
-						uint8_t fgcol = col & 0x0F;
-						uint32_t width = (end * font_width) - (start * font_width);
-						GDS_Box(start * font_width, line * font_height, width , font_height, getcolour(bgcol), getcolour(bgcol), 1, gds_LineStyle::Solid, gds_FillStyle::Filled);
-						string str = text.str();
-						for(size_t i = 0; i < str.length(); ++i){
-							if(str[i] <= 32) continue;
-							GDS_TextChar((start + i) * font_width, ((line + 1) * font_height) - (font_height / 5), str[i], font, fontSize, getcolour(fgcol));
-							++dchars;
-						}
-						//GDS_Text(start * font_width, line * font_height, text.str().c_str(), font, 0, getcolour(fgcol));
-						uint64_t drawend = bt_rtc_millis();
-						stringstream dss;
-						dss << "TW: Draw: " << drawend - drawstart << "ms (" << dchars << " chars)" << endl;
-						bt_zero(dss.str().c_str());
-					}
-					uint64_t s2s2 = bt_rtc_millis();
-					WM_UpdateRect((int32_t)(firststart * font_width), (int32_t)(line * font_height), (overallend - firststart + 1) * font_width, font_height);
-					uint64_t s2end = bt_rtc_millis();
-					stringstream s2ss;
-					s2ss << "TW: Stage2 1: " << s2s1 - s2start << "ms 2: " << s2s2 - s2s1 << "ms 3: " << s2end - s2s2 << "ms" << endl;
-					bt_zero(s2ss.str().c_str());
 				}
 				uint64_t end = bt_rtc_millis();
 				stringstream ss;
-				ss << "TW: stage1: " << stage1 - start << "ms stage2: " << end - stage1 << "ms" << endl;
+				ss << "TW: line drawn in " << end - start << "ms (" << ch << " changes)" << endl;
 				bt_zero(ss.str().c_str());
 			}
 		}
-		bt_rtc_sleep(100);
+		uint64_t updateStart = bt_rtc_millis();
+		WM_UpdateRect(updateRect);
+		uint64_t updateEnd = bt_rtc_millis();
+		stringstream uss;
+		uss << "TW: Prep: " << updateStart - prepStart << "ms Update: " << updateEnd - updateStart << "ms" << endl;
+		bt_zero(uss.str().c_str());
+		updateRect = {0, 0, 0, 0};
+		bt_rtc_sleep(50);
 	}
 }
 
@@ -442,7 +424,7 @@ int main(){
 	font_height = (info.maxH * fontSize) / info.scale;
 	{
 		stringstream ss;
-		ss << "TW: Scale:  " << info.scale << " width: " << info.maxW << " size: " << fontSize << endl;
+		ss << "TW: Scale: " << info.scale << " width: " << info.maxW << " size: " << fontSize << endl;
 		bt_zero(ss.str().c_str());
 	}
 	bt_terminial_init();
