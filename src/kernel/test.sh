730e6f19bf212c2c5e1db09b427150bd1d2bde3c
mallard <stuartbrockman@gmail.com>
2017-04-11 22:23:37 +0100

    Only use first line of qemu-launch-opts.txt

--- src/kernel/test.sh
+++ src/kernel/test.sh
@@ -1,3 +1,3 @@
 make -C .. &&
-qemu-system-i386 -s -cdrom ../iso/test/test.iso -m 128 -serial stdio -hda ../disk/btos.img -boot order=d $(cat ../qemu-launch-opts.txt)
+qemu-system-i386 -s -cdrom ../iso/test/test.iso -m 128 -serial stdio -hda ../disk/btos.img -boot order=d $(head -n 1 ../qemu-launch-opts.txt)
 #-d int,cpu_reset
