3c87985dcc7108af25aa7a5c3d1c75e699518378
Stuart Brockman <stuartbrockman@gmail.com>
2017-07-17 12:38:16 +0100

    Removed old commented out code from breakout.

--- src/user/wmapps/breakout/breakout.cpp
+++ src/user/wmapps/breakout/breakout.cpp
@@ -82,48 +82,5 @@ int main(){
 	msgLoop.AddHandler(eventLoop);
 	msgLoop.AddHandler(timer);
 	msgLoop.RunLoop();
-
-	/*Message msg = Message::Recieve();
-	while(true){
-		bt_msg_header head = msg.Header();
-		wm_Event e = WM_ParseMessage(&head);
-		if(e.type == wm_EventType::Close) break;
-		switch(state){
-			case gamestate::Title:{
-				if(e.type == wm_EventType::Keyboard) state = gamestate::GameStart;
-				break;
-			}
-			case gamestate::GameStart:{
-				InitGame();
-				state = gamestate::GamePlay;
-				bt_rtc_reset_timer(timer);
-				break;
-			}
-			case gamestate::GamePlay:{
-				if(msg.From() == 0 && msg.Source() == bt_rtc_ext_id){
-					bt_rtc_reset_timer(timer);
-					bool end = GameStep();
-					GameDraw();
-					if(end) state = gamestate::Finish;
-				}else{
-					GameEvent(e);
-				}
-				break;
-			}
-			case gamestate::Finish:{
-				EndGame();
-				DrawBackground();
-				DrawEndScreen();
-				WM_Update();
-				bt_rtc_sleep(2000);
-				DrawBackground();
-				DrawTitle();
-				WM_Update();
-				state = gamestate::Title;
-				break;
-			}
-		}
-		msg.Next();
-	}*/
     return 0;
 }
