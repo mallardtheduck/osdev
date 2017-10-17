3e0d7a3b627e3ecc25a06af4704257a174ea5eb7
Stuart Brockman <stuartbrockman@gmail.com>
2017-10-17 20:39:09 +0100

    Implemented full registry client API.

--- src/user/libs/registry/regapi.cpp
+++ src/user/libs/registry/regapi.cpp
@@ -8,11 +8,40 @@ using std::string;
 namespace btos_api{
 namespace registry{
 
-bt_pid_t pid;
+static bt_pid_t pid;
 
 vector<string> GetAllPackages(){
     return rpc::MakeClient<RPCID::GetAllPackages>(pid, GetAllPackages)();
 }
+
+PackageInfo GetPackageById(int64_t id){
+    return rpc::MakeClient<RPCID::GetPackageById>(pid, GetPackageById)(id);
+}
+
+PackageInfo GetPackageByName(const string &name){
+    return rpc::MakeClient<RPCID::GetPackageByName>(pid, GetPackageByName)(name);
+}
+
+vector<string> GetFeatures(int64_t pkgid){
+    return rpc::MakeClient<RPCID::GetFeatures>(pid, GetFeatures)(pkgid);
+}
+
+FeatureInfo GetFeatureById(int64_t id){
+    return rpc::MakeClient<RPCID::GetFeatureById>(pid, GetFeatureById)(id);
+}
+
+FeatureInfo GetFeatureByName(const string &name){
+    return rpc::MakeClient<RPCID::GetFeatureByName>(pid, GetFeatureByName)(name);
+}
+
+int64_t InstallPackage(const PackageInfo &info){
+    return rpc::MakeClient<RPCID::InstallPackage>(pid, InstallPackage)(info);
+}
+
+int64_t InstallFeature(const FeatureInfo &info){
+    return rpc::MakeClient<RPCID::InstallFeature>(pid, InstallFeature)(info);
+}
+
 
 }
 }
