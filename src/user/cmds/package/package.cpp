27ec20c516f5ba123c81ac03e0077d5f5dda5b13
Stuart Brockman <stuartbrockman@gmail.com>
2018-01-05 11:54:20 +0000

    Added CLIPP, modified to work, used in package utility.

--- src/user/cmds/package/package.cpp
+++ src/user/cmds/package/package.cpp
@@ -1,15 +1,45 @@
 #include <vector>
 #include <string>
+#include <iostream>
+#include <util/clipp.hpp>
 
 #include "package.hpp"
 
 using std::vector;
 using std::string;
 
-int main(int argc, char **argv){
-    vector<string> args{argv, argv + argc};
+using namespace clipp;
 
-    if(args[1] == "import" && args.size() == 4){
-        ImportPackage(args[2], args[3]);
-    }
+enum class Command{
+	Help,
+	Import
+};
+
+int main(int argc, char **argv){
+	Command cmd = Command::Help;
+	string file;
+	string path;
+	
+	auto importCmd = (
+		command("import").set(cmd, Command::Import),
+		value("info-file").set(file) % "Package info file to import",
+		value("install-path").set(path) % "Path to package install location"
+	);
+	
+	auto cli = (importCmd | command("help").set(cmd, Command::Help));
+	
+	if(parse(argc, argv, cli)){
+		switch(cmd){
+			case Command::Help:
+				std::cout << make_man_page(cli, argv[0]);
+				break;
+			case Command::Import:
+				ImportPackage(file, path);
+				break;
+		}
+	}else{
+		std::cout << usage_lines(cli, argv[0]) << std::endl;
+	}
+	
+    return 0;
 }
