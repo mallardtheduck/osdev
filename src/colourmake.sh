3a217131291059e40bbab69034a9b7bbb910fdad
Stuart Brockman <stuartbrockman@gmail.com>
2018-01-08 13:58:05 +0000

    Slight improvements to colourmake.sh

--- src/colourmake.sh
+++ src/colourmake.sh
@@ -7,7 +7,7 @@ m/error|No rule/i and print "\e[1;91m", "$_", "\e[0m"
 or
 m/warning/i and print "\e[1;93m", "$_", "\e[0m"
 or
-m/linking|\.a\b/ and print "\e[1;36m", "$_", "\e[0m"
+m/linking|cp|\.ld|\.elx|\.ell|\.a\b/ and print "\e[1;36m", "$_", "\e[0m"
 or
 print; '
 
