acb254a2666d968cab9ec07c059a13c28df0285f
Stuart Brockman <stuartbrockman@gmail.com>
2018-01-17 13:57:16 +0000

    Basic "dummy" port of SDL2.

---
+++ src/3rdparty/sdl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf ncurses-6.0
+if [ ! -f SDL2-2.0.7.tar.gz ];
+then
+	wget https://www.libsdl.org/release/SDL2-2.0.7.tar.gz
+fi
+tar xf SDL2-2.0.7.tar.gz
+mkdir -p originals
+tar xf SDL2-2.0.7.tar.gz -C originals
+
+cd SDL2-2.0.7
+cp ../sdl/Makefile .
+cp ../sdl/*.h include
+cp ../sdl/btos-build.sh .
+chmod +x btos-build.sh
+mkdir -p ../install
+./btos-build.sh
---
+++ src/3rdparty/sdl/btos-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ../../env-os.sh
+
+# architecture prefix
+ARCH="i686-pc-btos"
+
+# cumulative toolchain prefix
+PREFIX=/btos
+
+export CC=$ARCH-gcc
+export CXX=$ARCH-g++
+export LD=$ARCH-ld
+export NM="$ARCH-nm -B"
+export AR=$ARCH-ar
+export RANLIB=$ARCH-ranlib
+export STRIP=$ARCH-strip
+export OBJCOPY=$ARCH-objcopy
+export LN_S="ln -s"
+
+export CFLAGS="-g -O2"
+export CPPFLAGS=""
+export CXXFLAGS=""
+
+export LDFLAGS=""
+
+PATH=$BASE_PATH/bin:$PATH
+cp ../sdl/Makefile .
+make
