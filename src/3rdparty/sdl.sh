0b466298b59b26d85a0857ae8ed647ff7c4141f3
Stuart Brockman <stuartbrockman@gmail.com>
2018-04-06 10:36:46 +0100

    Quick port of SDL_ttf

--- src/3rdparty/sdl.sh
+++ src/3rdparty/sdl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf ncurses-6.0
+rm -rf SDL2-2.0.7
 if [ ! -f SDL2-2.0.7.tar.gz ];
 then
 	wget https://www.libsdl.org/release/SDL2-2.0.7.tar.gz
@@ -16,6 +16,22 @@ cp -Rv ../sdl/src/* src
 cp ../sdl/SDL_dynapi.h src/dynapi
 cp ../sdl/SDL_video.c src/video
 cp ../sdl/SDL_sysvideo.h src/video
+cp ../sdl/btos-build.sh .
+chmod +x btos-build.sh
+mkdir -p ../install
+./btos-build.sh
+
+cd ..
+rm -rf SDL2_ttf-2.0.14
+if [ ! -f SDL2_ttf-2.0.14.tar.gz ];
+then
+	wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
+fi
+tar xf SDL2_ttf-2.0.14.tar.gz
+mkdir -p originals
+tar xf SDL2_ttf-2.0.14.tar.gz -C originals
+cd SDL2_ttf-2.0.14
+cp ../sdl/ttf_Makefile Makefile
 cp ../sdl/btos-build.sh .
 chmod +x btos-build.sh
 mkdir -p ../install
