e20c9982dfe22bf7fb75596c966321efd33da691
mallard <stuartbrockman@gmail.com>
2018-04-25 21:04:00 +0100

    Added start of SDL2-widgets port.

---
+++ src/3rdparty/sdlwidgets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf SDL2-widgets-2.1
+if [ ! -f SDL2-widgets-2.1.tar.gz ];
+then
+	wget http://members.chello.nl/w.boeke/SDL-widgets/SDL2-widgets-2.1.tar.gz
+fi
+tar xf SDL2-widgets-2.1.tar.gz
+mkdir -p originals
+tar xf SDL2-widgets-2.1.tar.gz -C originals
+
+cd SDL2-widgets-2.1
+patch -p1 -R < ../sdlwidgets/sdlwidgets.patch
+make
