482e7a344674e5830cc7eae5d08b16cfa9d271ff
Stuart Brockman <stuartbrockman@gmail.com>
2018-06-10 20:18:09 +0100

    Implemented SDL timer support.

--- src/3rdparty/sdl/backup.sh
+++ src/3rdparty/sdl/backup.sh
@@ -3,6 +3,8 @@ cd "${BASH_SOURCE%/*}" || exit
 mkdir -p src/video/btos
 cp -Rv ../SDL2-2.0.7/src/video/btos/*.c src/video/btos
 cp -Rv ../SDL2-2.0.7/src/video/btos/*.h src/video/btos
+cp -Rv ../SDL2-2.0.7/src/timer/btos/*.c src/timer/btos
+cp -Rv ../SDL2-2.0.7/src/timer/btos/*.h src/timer/btos
 cp ../SDL2-2.0.7/src/dynapi/SDL_dynapi.h .
 cp ../SDL2-2.0.7/src/video/SDL_video.c .
 cp ../SDL2-2.0.7/src/video/SDL_sysvideo.h .
