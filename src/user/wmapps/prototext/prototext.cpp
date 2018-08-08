435f5bb2efe61a01688bc56112572e5d3569d657
Stuart Brockman <stuartbrockman@gmail.com>
2018-08-08 17:14:29 +0100

    Further work on prototype text editor.

--- src/user/wmapps/prototext/prototext.cpp
+++ src/user/wmapps/prototext/prototext.cpp
@@ -5,6 +5,7 @@
 
 static TopWin *top_win;
 static EditWin *edit_win;
+static VScrollbar *edit_scr;
 
 void set_icon(SDL_Window* window) {}
 
@@ -15,7 +16,9 @@ static void disp_topw() {
 
 int main(){
 	top_win = new TopWin("ProtoText", Rect(100, 100, 500, 500), 0, SDL_WINDOW_RESIZABLE, true, disp_topw, set_icon);
-	edit_win = new EditWin(top_win, Rect(0, 0, 500, 500), "", [](int ctrl_key,int key) {});
+	edit_win = new EditWin(top_win, Rect(0, 20, 490, 480), nullptr, [](int ctrl_key,int key) {});
+	edit_scr=new VScrollbar(top_win,Style(0,0,5), Rect(490, 20, 10, 480), 200, [](VScrollbar *sb) { edit_win->set_offset(sb->value); }
+  );
 	get_events();
 	return 0;
 }
