eb6ce796d761140f114ae16a7782010160ece48c
Stuart Brockman <stuartbrockman@gmail.com>
2018-09-05 14:01:45 +0100

    Make toolchain setup runnable in stages

--- src/setup-toolchain.sh
+++ src/setup-toolchain.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 export PREFIX="$HOME/Projects/os/cross"
 export TARGET=i686-pc-btos
 export PATH="$PREFIX/bin:$PATH"
@@ -37,47 +38,91 @@ then
 	
 fi
 
-pushd gcc-4.8.1/libstdc++-v3 && \
-autoconf2.64 && \
-popd && \
-\
-# pushd newlib-2.1.0/newlib/libc/sys && \
-# autoconf && \
-# cd btos && \
-# autoreconf && \
-# popd && \
-# \
-cd $HOME/Projects/os/src
-rm -rf build-binutils
-mkdir build-binutils && \
-cd build-binutils && \
-../binutils-2.23/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-werror && \
-make && \
-make install && \
-\
-rm -rf "$PREFIX/lib/gcc/i686-pc-btos/4.8.1" && \
-cd $HOME/Projects/os/src && \
-rm -rf build-gcc
-mkdir build-gcc && \
-cd build-gcc && \
-../gcc-4.8.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --with-newlib --disable-multilib --enable-shared=libgcc,libstdc++ --enable-initfini-array && \
-make all-gcc && \
-make install-gcc && \
-make all-target-libgcc
-cp i686-pc-btos/libgcc/libgcc.a "$PREFIX/i686-pc-btos/lib"
-SHLIB_LINK="i686-pc-btos-gcc -O2 -fPIC -shared @shlib_objs@ -o @shlib_base_name@.ell" make all-target-libgcc && \
-#make install-gcc && \
-make -C ../user/libs/newlib/libc startfiles && \
-make install-target-libgcc && \
-find i686-pc-btos/libgcc -name \*.ell -exec cp {} ../../cross/i686-pc-btos/lib \; && \
-\
-cd .. && \
-make -C ../user/libs/newlib/libc copy-includes && \
-make newlib && \
-cd build-gcc && \
-\
-cd $HOME/Projects/os/src/build-gcc && \
-mkdir -p i686-pc-btos/libstdc++-v3 && \
-cp ../toolchain/misc/libtool i686-pc-btos/libstdc++-v3 && \
-make && \
-make install
+case "$2" in
+	stage1)
+	run=1
+	;;
+	
+	stage2)
+	run=2
+	;;
+	
+	stage3)
+	run=3
+	;;
+	
+	stage4)
+	run=4
+	;;
+	
+	*)
+	run=1234
+	;;
+esac
+
+case "$run" in
+	*1*)
+
+	pushd gcc-4.8.1/libstdc++-v3 && \
+	autoconf2.64 && \
+	popd && \
+	\
+	# pushd newlib-2.1.0/newlib/libc/sys && \
+	# autoconf && \
+	# cd btos && \
+	# autoreconf && \
+	# popd && \
+	# \
+	cd $HOME/Projects/os/src
+	rm -rf build-binutils
+	mkdir build-binutils && \
+	cd build-binutils && \
+	../binutils-2.23/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-werror && \
+	make && \
+	make install && \
+	\
+	rm -rf "$PREFIX/lib/gcc/i686-pc-btos/4.8.1" && \
+	cd $HOME/Projects/os/src && \
+	rm -rf build-gcc
+	mkdir build-gcc && \
+	cd build-gcc && \
+	../gcc-4.8.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --with-newlib --disable-multilib --enable-shared=libgcc,libstdc++ --enable-initfini-array && \
+	make all-gcc && \
+	make install-gcc
+	;;
+esac
+
+case "$run" in
+	*2*)
+
+	make -C ../user/libs/newlib/libc startfiles && \
+	make all-target-libgcc
+	cp i686-pc-btos/libgcc/libgcc.a "$PREFIX/i686-pc-btos/lib"
+	SHLIB_LINK="i686-pc-btos-gcc -O2 -fPIC -shared @shlib_objs@ -o @shlib_base_name@.ell" make all-target-libgcc && \
+	make install-target-libgcc && \
+	find i686-pc-btos/libgcc -name \*.ell -exec cp {} ../../cross/i686-pc-btos/lib \;
+	;;
+esac
+
+
+case "$run" in
+	*3*)
+	
+	cd .. && \
+	make -C user/libs/newlib/libc copy-includes && \
+	make newlib
+	;;
+esac
+
+case "$run" in
+	*4*)
+	
+	cd build-gcc && \
+	\
+	cd $HOME/Projects/os/src/build-gcc && \
+	mkdir -p i686-pc-btos/libstdc++-v3 && \
+	cp ../toolchain/misc/libtool i686-pc-btos/libstdc++-v3 && \
+	make && \
+	make install
+	;;
+esac
