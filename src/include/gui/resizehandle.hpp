0e83f537e6bc7c74e326845837c4beda3b92e57a
Stuart Brockman <stuartbrockman@gmail.com>
2019-06-03 15:57:13 +0000

    Added "ResizeHandle" control

---
+++ src/include/gui/resizehandle.hpp
@@ -0,0 +1,42 @@
+#ifndef _RESIZEHANDLE_HPP
+#define _RESIZEHANDLE_HPP
+
+#include "icontrol.hpp"
+
+namespace btos_api{
+namespace gui{
+
+class ResizeHandle : public IActionControl<void>{
+private:
+	bool down = false;
+	bool enabled = true;
+	bool paintDown;
+
+	gds::Rect rect;
+	
+	std::unique_ptr<gds::Surface> surf;
+	std::unique_ptr<gds::Surface> bkSurf;
+	gds_SurfaceInfo info;
+	
+public:
+	ResizeHandle(const gds::Rect &r);
+
+	EventResponse HandleEvent(const wm_Event&);
+	void Paint(gds::Surface &s);
+	gds::Rect GetPaintRect();
+	gds::Rect GetInteractRect();
+	uint32_t GetSubscribed();
+	void Focus();
+	void Blur();
+	uint32_t GetFlags();
+	void Enable();
+	void Disable();
+	bool IsEnabled();
+	
+	bool GetValue();
+};
+
+}
+}
+
+#endif
