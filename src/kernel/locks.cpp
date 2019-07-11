851224d16737d51ed887d19ffccb5476b7ee60f4
Stuart Brockman <stuartbrockman@gmail.com>
2019-07-11 14:12:28 +0000

    Let's not rely on the lock class/struct never changing...

--- src/kernel/locks.cpp
+++ src/kernel/locks.cpp
@@ -40,7 +40,7 @@ void take_lock_exclusive(lock &l, uint64_t thread, bool us){
     if(thread==0) panic("(LOCK) Attempt to lock thread to invalid thread ID.");
     while(l.lockval != thread) {
         while (!__sync_bool_compare_and_swap(&l.lockval, 0, thread)) {
-            if (&l != &sch_lock && sch_lock.lockval != thread) sch_setblock(&lock_blockcheck, (void *) &l.lockval, l.lockval);
+            if (&l != &sch_lock && sch_lock.lockval != thread) sch_setblock(&lock_blockcheck, (void*)&l, l.lockval);
             else panic("(LOCK) Deadlock - waiting for lock while holding scheduler lock!");
         }
     }
@@ -61,7 +61,7 @@ void take_lock_recursive(lock &l, uint64_t thread){
     if(thread==0) panic("(LOCK) Attempt to lock thread to invalid thread ID.");
     while(l.lockval != thread) {
         while (!__sync_bool_compare_and_swap(&l.lockval, 0, thread)) {
-            if (&l != &sch_lock && sch_lock.lockval != thread) sch_setblock(&lock_blockcheck, (void *) &l.lockval, l.lockval);
+            if (&l != &sch_lock && sch_lock.lockval != thread) sch_setblock(&lock_blockcheck, (void*)&l, l.lockval);
             else panic("(LOCK) Deadlock - waiting for lock while holding scheduler lock!");
         }
     }
