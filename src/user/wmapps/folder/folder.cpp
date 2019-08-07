5818646c7a6f16d3d257ab7dfc0c7abc20fd332b
Stuart Brockman <stuartbrockman@gmail.com>
2019-08-07 16:11:01 +0000

    Refactored Folder View to use ActionManager

--- src/user/wmapps/folder/folder.cpp
+++ src/user/wmapps/folder/folder.cpp
@@ -11,6 +11,7 @@
 #include <gui/statusbar.hpp>
 #include <gui/messagebox.hpp>
 #include <gui/inputbox.hpp>
+#include <gui/actionmanager.hpp>
 
 #include <wm/eventloop.hpp>
 #include <wm/menu.hpp>
@@ -56,32 +57,15 @@ int main(int argc, char **argv){
 	std::shared_ptr<wm::Menu> editMenu;
 	std::function<void()> updateMenus;
 	
+	std::shared_ptr<gui::ToolbarButton> backBtn;
+	std::shared_ptr<gui::ToolbarButton> fwdBtn;
+	std::shared_ptr<gui::ToolbarButton> upBtn;
+	std::shared_ptr<gui::ToolbarButton> iconsBtn;
+	std::shared_ptr<gui::ToolbarButton> detailsBtn;
+	
 	auto form = std::make_shared<gui::Form>(gds::Rect{50, 50, 500, 320}, gui::FormOptions::ClosedSizable, sh::PathItemTitle(curPath));
 	
 	auto toolbar = std::make_shared<gui::Toolbar>();
-	auto newWinBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/window_new_16.png"));
-	auto treeBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/view_tree_16.png"));
-	auto iconsBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/view_icon_16.png"));
-	auto detailsBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/view_detailed_16.png"));
-	auto spc1 = std::make_shared<gui::ToolbarSpacer>();
-	auto upBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/up_16.png"));
-	auto backBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/back_16.png"));
-	auto fwdBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/forward_16.png"));
-	auto spc2 = std::make_shared<gui::ToolbarSpacer>();
-	auto newFolderBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/new_folder_16.png"));
-	auto editPasteBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/editpaste_16.png"));
-	auto editCopyBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/editcopy_16.png"));
-	auto editCutBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/editcut_16.png"));
-	auto deleteBtn = std::make_shared<gui::ToolbarButton>(LoadIcon("icons/editdelete_16.png"));
-	
-	toolbar->Controls() = {newWinBtn, treeBtn, iconsBtn, detailsBtn, spc1, upBtn, backBtn, fwdBtn, spc2, 
-		newFolderBtn, editPasteBtn, editCopyBtn, editCutBtn, deleteBtn};
-	toolbar->Refresh();
-	
-	iconsBtn->Disable();
-	upBtn->Disable();
-	backBtn->Disable();
-	fwdBtn->Disable();
 	
 	auto tree = std::make_shared<sh::FolderTreeView>(gds::Rect{margin, margin + toolbarHeight, treeWidth, contentHeight}, "");
 	auto icons = std::make_shared<sh::FolderIconView>(gds::Rect{treeWidth + (2 * margin), margin + toolbarHeight, formWidth - treeWidth - (3 * margin), contentHeight}, "");
@@ -163,8 +147,6 @@ int main(int argc, char **argv){
 		updateSize({0, 0, curWidth, curHeight});
 	};
 	
-	treeBtn->OnAction(toggleTree);
-	
 	auto iconView = [&]{
 		curView = ViewMode::Icons;
 		form->RemoveControl(details);
@@ -174,8 +156,6 @@ int main(int argc, char **argv){
 		updateMenus();
 	};
 	
-	iconsBtn->OnAction(iconView);
-	
 	auto detailsView = [&]{
 		curView = ViewMode::Details;
 		form->RemoveControl(icons);
@@ -184,8 +164,6 @@ int main(int argc, char **argv){
 		iconsBtn->Enable();
 		updateMenus();
 	};
-	
-	detailsBtn->OnAction(detailsView);
 	
 	tree->OnChange([&](gui::TreeViewNode*){
 		auto path = tree->GetSelectedPath();
@@ -227,8 +205,6 @@ int main(int argc, char **argv){
 		}
 	};
 	
-	backBtn->OnAction(goBack);
-	
 	auto goFwd = [&]{
 		if(!future.empty()){
 			auto path = future.top();
@@ -237,8 +213,6 @@ int main(int argc, char **argv){
 			navigateTo(path, false);
 		}
 	};
-	
-	fwdBtn->OnAction(goFwd);
 	
 	auto goUp = [&]{
 		if(!curPath.empty()){
@@ -249,13 +223,9 @@ int main(int argc, char **argv){
 		}
 	};
 	
-	upBtn->OnAction(goUp);
-	
 	auto newWindow = [&]{
 		if(argc) btos_api::bt_spawn(argv[0], 0, nullptr);
 	};
-	
-	newWinBtn->OnAction(newWindow);
 	
 	auto newFolder = [&]{
 		if(curPath != ""){
@@ -278,53 +248,51 @@ int main(int argc, char **argv){
 		}
 	};
 	
-	newFolderBtn->OnAction(newFolder);
+	auto about = [&]{
+		gui::MessageBox("BT/OS Folder View", "About", LoadIcon("icons/folder_32.png")).Show(form.get());
+	};
+	
+	gui::ActionManager am;
+	am.Add("newWindow", "New window...", LoadIcon("icons/window_new_16.png"), newWindow);
+	am.Add("toggleTree", "Toggle tree", LoadIcon("icons/view_tree_16.png"), toggleTree);
+	am.Add("iconView", "Icon view", LoadIcon("icons/view_icon_16.png"), iconView);
+	am.Add("detailsView", "Details view", LoadIcon("icons/view_detailed_16.png"), detailsView);
+	am.Add("goUp", "Up a level", LoadIcon("icons/up_16.png"), goUp);
+	am.Add("goBack", "Back", LoadIcon("icons/back_16.png"), goBack);
+	am.Add("goFwd", "Forward", LoadIcon("icons/forward_16.png"), goFwd);
+	am.Add("newFolder", "New folder...", LoadIcon("icons/new_folder_16.png"), newFolder);
+	am.Add("editPaste", "Paste", LoadIcon("icons/editpaste_16.png"), []{});
+	am.Add("editCopy", "Copy", LoadIcon("icons/editcopy_16.png"), []{});
+	am.Add("editCut", "Cut", LoadIcon("icons/editcut_16.png"), []{});
+	am.Add("delete", "Delete", LoadIcon("icons/editdelete_16.png"), []{});
+	am.Add("about", "About...", LoadIcon("icons/folder_16.png"), about);
+	
+	auto newWinBtn = am.GetToolbarButton("newWindow");
+	auto treeBtn = am.GetToolbarButton("toggleTree");
+	iconsBtn = am.GetToolbarButton("iconView");
+	detailsBtn = am.GetToolbarButton("detailsView");
+	auto spc1 = std::make_shared<gui::ToolbarSpacer>();
+	upBtn = am.GetToolbarButton("goUp");
+	backBtn = am.GetToolbarButton("goBack");
+	fwdBtn = am.GetToolbarButton("goFwd");
+	auto spc2 = std::make_shared<gui::ToolbarSpacer>();
+	auto newFolderBtn = am.GetToolbarButton("newFolder");
+	auto editPasteBtn = am.GetToolbarButton("editPaste");
+	auto editCopyBtn = am.GetToolbarButton("editCopy");
+	auto editCutBtn = am.GetToolbarButton("editCut");
+	auto deleteBtn = am.GetToolbarButton("delete");
+	
+	toolbar->Controls() = {newWinBtn, treeBtn, iconsBtn, detailsBtn, spc1, upBtn, backBtn, fwdBtn, spc2, 
+		newFolderBtn, editPasteBtn, editCopyBtn, editCutBtn, deleteBtn};
+	toolbar->Refresh();
+	
+	iconsBtn->Disable();
+	upBtn->Disable();
+	backBtn->Disable();
+	fwdBtn->Disable();
 	
 	auto menuHandler = [&](int action) -> bool{
-		switch(action){
-			case 1:
-				newWindow();
-				break;
-			case 2:
-				newFolder();
-				break;
-			case 3:
-				gui::MessageBox("BT/OS Folder View", "About", LoadIcon("icons/folder_32.png")).Show(form.get());
-				break;
-				
-			case 11:
-				toggleTree();
-				break;
-			case 12:
-				iconView();
-				break;
-			case 13:
-				detailsView();
-				break;
-			
-			case 21:
-				goBack();
-				break;
-			case 22:
-				goFwd();
-				break;
-			case 23:
-				goUp();
-				break;
-			
-			case 31:
-				//paste
-				break;
-			case 32:
-				//copy
-				break;
-			case 33:
-				//cut
-				break;
-			case 34:
-				//delete
-				break;
-		}
+		am.MenuHandler(action);
 		return true;
 	};
 	
@@ -333,44 +301,50 @@ int main(int argc, char **argv){
 	updateMenus = [&]{
 		if(viewMenu) loop.RemoveMenu(viewMenu->GetID());
 		viewMenu = std::make_shared<wm::Menu>();
-		viewMenu->AddItem(wm::MenuItem(11, "Toggle tree", wm_MenuItemFlags::Default, LoadIcon("icons/view_tree_16.png").get()));
+		viewMenu->AddItem(am.GetMenuItem("toggleTree"));
 		viewMenu->AddItem(wm::MenuItem(0, "", wm_MenuItemFlags::Separator));
-		auto iconFlags = curView == ViewMode::Icons ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
-		viewMenu->AddItem(wm::MenuItem(12, "Icon view",iconFlags, LoadIcon("icons/view_icon_16.png").get()));
-		auto detailFlags = curView == ViewMode::Details ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
-		viewMenu->AddItem(wm::MenuItem(13, "Detail view", detailFlags, LoadIcon("icons/view_detailed_16.png").get()));
+		auto iconsItem = am.GetMenuItem("iconView");
+		iconsItem.flags = curView == ViewMode::Icons ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
+		viewMenu->AddItem(iconsItem);
+		auto detailsItem = am.GetMenuItem("detailsView");
+		detailsItem.flags = curView == ViewMode::Details ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
+		viewMenu->AddItem(detailsItem);
 		viewMenu->SetEventHandler(menuHandler);
 		loop.AddMenu(viewMenu);
 		
 		if(navMenu) loop.RemoveMenu(navMenu->GetID());
 		navMenu = std::make_shared<wm::Menu>();
-		auto backFlags = history.empty() ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
-		navMenu->AddItem(wm::MenuItem(21, "Back", backFlags, LoadIcon("icons/back_16.png").get()));
-		auto forwardFlags = future.empty() ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
-		navMenu->AddItem(wm::MenuItem(22, "Forward", forwardFlags, LoadIcon("icons/forward_16.png").get()));
-		auto upFlags = curPath.empty() ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
-		navMenu->AddItem(wm::MenuItem(23, "Up a level", upFlags, LoadIcon("icons/up_16.png").get()));
+		auto backItem = am.GetMenuItem("goBack");
+		backItem.flags = history.empty() ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
+		navMenu->AddItem(backItem);
+		auto forwardItem = am.GetMenuItem("goFwd");
+		forwardItem.flags = future.empty() ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
+		navMenu->AddItem(forwardItem);
+		auto upItem = am.GetMenuItem("goUp");
+		upItem.flags = curPath.empty() ? wm_MenuItemFlags::Disabled : wm_MenuItemFlags::Default;
+		navMenu->AddItem(upItem);
 		navMenu->SetEventHandler(menuHandler);
 		loop.AddMenu(navMenu);
 		
 		if(editMenu) loop.RemoveMenu(editMenu->GetID());
 		editMenu = std::make_shared<wm::Menu>();
-		editMenu->AddItem(wm::MenuItem(31, "Paste", wm_MenuItemFlags::Default, LoadIcon("icons/editpaste_16.png").get()));
-		editMenu->AddItem(wm::MenuItem(32, "Copy", wm_MenuItemFlags::Default, LoadIcon("icons/editcopy_16.png").get()));
-		editMenu->AddItem(wm::MenuItem(33, "Cut", wm_MenuItemFlags::Default, LoadIcon("icons/editcut_16.png").get()));
-		editMenu->AddItem(wm::MenuItem(34, "Delete", wm_MenuItemFlags::Default, LoadIcon("icons/editdelete_16.png").get()));
+		editMenu->AddItem(am.GetMenuItem("editPaste"));
+		editMenu->AddItem(am.GetMenuItem("editCopy"));
+		editMenu->AddItem(am.GetMenuItem("editCut"));
+		editMenu->AddItem(am.GetMenuItem("delete"));
 		editMenu->SetEventHandler(menuHandler);
 		loop.AddMenu(editMenu);
 		
 		if(menu) loop.RemoveMenu(menu->GetID());
 		menu = std::make_shared<wm::Menu>();
-		menu->AddItem(wm::MenuItem(1, "New window", wm_MenuItemFlags::Default, LoadIcon("icons/window_new_16.png").get()));
-		menu->AddItem(wm::MenuItem(2, "New folder...", wm_MenuItemFlags::Default, LoadIcon("icons/new_folder_16.png").get()));
+		menu->AddItem(am.GetMenuItem("newWindow"));
+		menu->AddItem(am.GetMenuItem("newFolder"));
 		menu->AddItem(wm::MenuItem(0, "View", wm_MenuItemFlags::ChildMenu, nullptr, viewMenu.get()));
 		menu->AddItem(wm::MenuItem(0, "Navigate", wm_MenuItemFlags::ChildMenu, nullptr, navMenu.get()));
 		menu->AddItem(wm::MenuItem(0, "Edit", wm_MenuItemFlags::ChildMenu, nullptr, editMenu.get()));
 		menu->AddItem(wm::MenuItem(0, "", wm_MenuItemFlags::Separator));
-		menu->AddItem(wm::MenuItem(3, "About...", wm_MenuItemFlags::Default, LoadIcon("icons/folder_16.png").get()));
+		menu->AddItem(am.GetMenuItem("about"));
+		
 		menu->SetEventHandler(menuHandler);
 		loop.AddMenu(menu);
 		
