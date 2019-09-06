1c0386ad8d7d364c3027fd0ad98fe41fd94b2322
Stuart Brockman <stuartbrockman@gmail.com>
2019-09-06 11:37:45 +0000

    Ensure dialog boxes are displayed entirly on screen.

--- src/user/libs/gui/idialog.cpp
+++ src/user/libs/gui/idialog.cpp
@@ -1,11 +1,11 @@
 #include <gui/idialog.hpp>
-#include <util/tinyformat.hpp>
 
 namespace btos_api{
 namespace gui{
 	
 gds::Point DialogPosition(wm::Window *parent, const wm::Window &form){
 	auto info = form.Info();
+	auto smode = form.GetScreenMode();
 	auto contentInfo = gds::Surface::Wrap(info.gds_id, false).Info();
 	int32_t totalFormWidth = contentInfo.w + info.contentX;
 	int32_t totalFormHeight = contentInfo.h + info.contentY;
@@ -21,10 +21,15 @@ gds::Point DialogPosition(wm::Window *parent, const wm::Window &form){
 		
 		int32_t formX = pInfo.x + formOffsetX;
 		int32_t formY = pInfo.y + formOffsetY;
+
+		if(formX + totalFormWidth > (int32_t)smode.width) formX = smode.width - totalFormWidth;
+		if(formX < 0) formX = 0;
+
+		if(formY + totalFormHeight > (int32_t)smode.height) formY = smode.height - totalFormHeight;
+		if(formY < 0) formY = 0;
 		
 		return {formX, formY};
 	}else{
-		auto smode = form.GetScreenMode();
 		int32_t formX = (smode.width - totalFormWidth) / 2;
 		int32_t formY = (smode.height - totalFormHeight) / 2;
 		return {formX, formY};
