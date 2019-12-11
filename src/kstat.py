d656b1b78a30c61facbde61a20cb0675dbd22fb3
Stuart Brockman <stuartbrockman@gmail.com>
2019-12-11 23:18:42 +0000

    Display correct module names when module addresses are not consistent with load order.

--- src/kstat.py
+++ src/kstat.py
@@ -1,45 +1,51 @@
 class ThreadsCommand (gdb.Command):
 
-  def __init__ (self):
-    super (ThreadsCommand, self).__init__ ("btos-threadinfo", gdb.COMMAND_USER)
-    
-  def pid_to_name(self, pid):
-  	procCount = gdb.parse_and_eval("proc_processes.dataSize")
-  	for pix in range(procCount):
-  		cpid = gdb.parse_and_eval("proc_processes.data[%s]->pid" % pix)
-  		if cpid == pid: return gdb.parse_and_eval("proc_processes.data[%s]->name.p" % pix)
-  	return "Unknown PID"
-  	
-  def addr_to_module(self, addr):
-  	modCount = gdb.parse_and_eval("loaded_modules.dataSize")
-  	ret = "KERNEL"
-  	for mix in range(modCount):
-  		base = gdb.parse_and_eval("loaded_modules.data[%s].elf.mem.aligned" % mix)
-  		if base < addr: ret = gdb.parse_and_eval("loaded_modules.data[%s].filename.p" % mix)
-  		else: return ret
-  	return ret
+	def __init__ (self):
+		super (ThreadsCommand, self).__init__ ("btos-threadinfo", gdb.COMMAND_USER)
+		
+	def pid_to_name(self, pid):
+		procCount = gdb.parse_and_eval("proc_processes.dataSize")
+		for pix in range(procCount):
+			cpid = gdb.parse_and_eval("proc_processes.data[%s]->pid" % pix)
+			if cpid == pid: return gdb.parse_and_eval("proc_processes.data[%s]->name.p" % pix)
+		return "Unknown PID"
+		
+	def addr_to_module(self, addr):
+		modCount = gdb.parse_and_eval("loaded_modules.dataSize")
+		ret = "KERNEL"
+		modules = []
+		for mix in range(modCount):
+			base = gdb.parse_and_eval("loaded_modules.data[%s].elf.mem.aligned" % mix)
+			name = gdb.parse_and_eval("loaded_modules.data[%s].filename.p" % mix)
+			modules.append({'base': base, 'name': name})
+		modules.sort(key=lambda x: x['base'])
 
-  def invoke (self, arg, from_tty):
-    threadCount = gdb.parse_and_eval("threads.dataSize")
-    for tix in range(threadCount):
-    	tid = gdb.parse_and_eval("threads.data[%s]->ext_id" % tix)
-    	pid = gdb.parse_and_eval("threads.data[%s]->pid" % tix)
-    	status = gdb.parse_and_eval("threads.data[%s]->status" % tix)
-    	bc = gdb.parse_and_eval("threads.data[%s]->blockcheck" % tix)
-    	blockinfo = ""
-    	if bc != 0:
-    		modname = self.addr_to_module(bc)
-    		bcp = gdb.parse_and_eval("threads.data[%s]->bc_param" % tix)
-    		blockinfo = "(%s :: %s (%s))" % (modname, bc, bcp)
-    	name = self.pid_to_name(pid)
-    	print("%s: TID: %s PID: %s (%s) Status: %s %s" % (tix, tid, pid, name, status, blockinfo));
+		for mod in modules:
+			if mod['base'] < addr: ret = mod['name'] 
+			else: return ret
+		return ret
+
+	def invoke (self, arg, from_tty):
+		threadCount = gdb.parse_and_eval("threads.dataSize")
+		for tix in range(threadCount):
+			tid = gdb.parse_and_eval("threads.data[%s]->ext_id" % tix)
+			pid = gdb.parse_and_eval("threads.data[%s]->pid" % tix)
+			status = gdb.parse_and_eval("threads.data[%s]->status" % tix)
+			bc = gdb.parse_and_eval("threads.data[%s]->blockcheck" % tix)
+			blockinfo = ""
+			if bc != 0:
+				modname = self.addr_to_module(bc)
+				bcp = gdb.parse_and_eval("threads.data[%s]->bc_param" % tix)
+				blockinfo = "(%s :: %s (%s))" % (modname, bc, bcp)
+			name = self.pid_to_name(pid)
+			print("%s: TID: %s PID: %s (%s) Status: %s %s" % (tix, tid, pid, name, status, blockinfo))
 
 ThreadsCommand ()
 
 class ModsCommand (gdb.Command):
 	def __init__ (self):
 	   super (ModsCommand, self).__init__ ("btos-modinfo", gdb.COMMAND_USER)
-    
+	
 	def invoke (self, arg, from_tty):
 		modCount = gdb.parse_and_eval("loaded_modules.dataSize")
 		for mix in range(modCount):
@@ -52,7 +58,7 @@ ModsCommand ()
 class ProcsCommand (gdb.Command):
 	def __init__ (self):
 	   super (ProcsCommand, self).__init__ ("btos-procinfo", gdb.COMMAND_USER)
-    
+	
 	def invoke (self, arg, from_tty):
 	  	procCount = gdb.parse_and_eval("proc_processes.dataSize")
 	  	for pix in range(procCount):
