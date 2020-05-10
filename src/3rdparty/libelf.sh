018d7ad39bd92221895659e955d09bf6e1821cf8
Stuart Brockman <stuartbrockman@gmail.com>
2020-05-10 16:25:08 +0000

    Update libelf port.

--- src/3rdparty/libelf.sh
+++ src/3rdparty/libelf.sh
@@ -3,13 +3,13 @@
 rm -rf libelf-0.8.9
 if [ ! -f libelf-0.8.9.tar.gz ];
 then
-	wget https://github.com/mallardtheduck/btos-3rdparty-prereqs/raw/master/libelf-0.8.9.tar.gz
+	wget https://github.com/mallardtheduck/btos-3rdparty-prereqs/raw/master/libelf-0.8.13.tar.gz
 fi
-tar xf libelf-0.8.9.tar.gz
+tar xf libelf-0.8.13.tar.gz
 mkdir -p originals
-tar xf libelf-0.8.9.tar.gz -C originals
+tar xf libelf-0.8.13.tar.gz -C originals
 
-cd libelf-0.8.9
+cd libelf-0.8.13
 patch -p1 -R < ../libelf/libelf.patch
 cp ../libelf/btos-build.sh .
 mkdir -p install
