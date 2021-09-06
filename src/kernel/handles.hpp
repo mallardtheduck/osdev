5a3f4a96b306ccc8ba1191738b923b43f0e9f559
Stuart Brockman <stuartbrockman@gmail.com>
2021-09-06 20:51:57 +0000

    Remove some old function prototypes and add GenericHandle<T> to replace old-style handles.

--- src/kernel/handles.hpp
+++ src/kernel/handles.hpp
@@ -4,31 +4,27 @@
 #include <module/handle.h>
 
 namespace kernel_handle_types{
-    static const uint32_t invalid=0;
-    static const uint32_t lock=1;
-    static const uint32_t file=2;
-    static const uint32_t directory=3;
-    static const uint32_t thread=4;
-    static const uint32_t memory_mapping=5;
+	static const uint32_t invalid=0;
+	static const uint32_t lock=1;
+	static const uint32_t file=2;
+	static const uint32_t directory=3;
+	static const uint32_t thread=4;
+	static const uint32_t memory_mapping=5;
 	static const uint32_t shm_space=6;
 	static const uint32_t shm_mapping=7;
 	static const uint32_t atom=8;
 	static const uint32_t wait=9;
 	static const uint32_t atomwait=10;
 	static const uint32_t msg_recv=11;
-    static const uint32_t elf = 12;
+	static const uint32_t elf = 12;
 };
 
 void init_handles();
 
-bt_handle_info create_handle(uint32_t type, void *value, handle_close_fn close, handle_wait_fn wait = nullptr);
-void close_handle(bt_handle_info &handle);
-void wait_handle(bt_handle_info &handle);
-
 void add_handle_dep(bt_handle_info &h);
 
 enum class handle_dep_check_result{
-    Present, Absent, NotAvailable
+	Present, Absent, NotAvailable
 };
 
 handle_dep_check_result handle_dep_check(bt_handle_info &h);
@@ -37,14 +33,47 @@ extern bt_handle_info invalid_handle;
 
 class IHandle{
 public:
-    virtual uint32_t GetType() = 0;
+	virtual uint32_t GetType() = 0;
 
-    virtual void Close() = 0;
-    virtual bool Wait() = 0;
+	virtual void Close() = 0;
+	virtual bool Wait() = 0;
 
-    virtual ~IHandle(){}
+	virtual ~IHandle(){}
 };
 
-bt_handle_info create_handle(IHandle *c);
+template<typename T>
+class GenericHandle : public IHandle{
+protected:
+	uint32_t type;
+	T data;
+	function<void(T)> close;
+	function<bool(T)> wait;
+public:
+	GenericHandle(uint32_t t, T *d, function<void(T)> c, function<bool(T)> w) : type(t), data(d), close(c), wait(w) {}
+
+	virtual uint32_t GetType() override{
+		return type;
+	}
+
+	virtual void Close() override{
+		if(close) close(data);
+	}
+
+	virtual bool Wait() override{
+		if(wait) return wait(data);
+		else return false;
+	}
+
+	virtual T &GetData(){
+		return data;
+	}
+
+	virtual ~GenericHandle() {}
+};
+
+template<typename T>
+GenericHandle<T> *MakeGenericHandle(uint32_t type, T data, function<void(T)> close = nullptr, function<bool(T)> wait = nullptr){
+	return GenericHandle<T>(type, data, close, wait);
+}
 
 #endif
