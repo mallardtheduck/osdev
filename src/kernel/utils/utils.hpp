8d698fb31a905212e5e73f4028bee74a097b5460
Stuart Brockman <stuartbrockman@gmail.com>
2021-09-06 20:59:19 +0000

    Update utils for C++17 new/delete operators. Add min/max templates.

--- src/kernel/utils/utils.hpp
+++ src/kernel/utils/utils.hpp
@@ -14,27 +14,45 @@ vector<string> split_string(const string &str, const char c);
 //C++ new/delete operators
 inline void *operator new(size_t size)
 {
-    return malloc(size);
+	return malloc(size);
 }
  
 inline void *operator new[](size_t size)
 {
-    return malloc(size);
+	return malloc(size);
 }
  
 inline void operator delete(void *p)
 {
-    free(p);
+	free(p);
+}
+
+inline void operator delete(void *p, size_t)
+{
+	free(p);
 }
  
 inline void operator delete[](void *p)
 {
-    free(p);
+	free(p);
+}
+
+inline void operator delete[](void *p, size_t)
+{
+	free(p);
 }
 
 template<typename T> void *operator new (size_t, T* ptr)
 {
 	return ptr;
+}
+
+template<typename T> T min(T a, T b){
+	return (a < b) ? a : b;
+}
+
+template<typename T>  T max(T a, T b){
+	return (a > b) ? a : b;
 }
 
 #endif
