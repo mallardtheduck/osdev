50d313d61aa1299c770ad09a495c8b373e22dddc
Stuart Brockman <stuartbrockman@gmail.com>
2021-09-14 15:35:19 +0000

    Remove old kvars API.

--- src/kernel/kvars.hpp
+++ src/kernel/kvars.hpp
@@ -15,11 +15,4 @@ public:
 void KernelConfigVariables_Init();
 IKernelConfigVariables &GetKernelConfigVariables();
 
-class string;
-
-void init_kvars();
-void set_kvar(const string &name, const string &value);
-string get_kvar(const string &name);
-bool is_kvar_set(const string &name);
-
 #endif
