329e6e3690dea420a0fa1faae823f048577c7c5b
Stuart Brockman <stuartbrockman@gmail.com>
2021-12-14 15:10:46 +0000

    Ported ISO9660 filesystem to new API.

--- src/modules/fs/iso9660/iso9660.cpp
+++ src/modules/fs/iso9660/iso9660.cpp
@@ -1,5 +1,7 @@
 #include <module/module.inc>
 #include <module/utils/string.hpp>
+#include <module/utils/fshelpers.hpp>
+
 #include <util/scoped_ptr.hpp>
 
 extern "C"{
@@ -10,6 +12,84 @@ extern "C"{
 
 constexpr size_t block_size = 2048;
 
+using fs_path = vector<string>;
+
+static fs_path split_path(const string &path){
+	fs_path ret;
+	string cur;
+	for(auto c : path){
+		if(c == '/'){
+			if(!cur.empty()) ret.push_back(cur);
+			cur.clear();
+		}else{
+			cur += c;
+		}
+	}
+	if(!cur.empty()) ret.push_back(cur);
+	return ret;
+}
+
+static string shorten(const string &fname){
+	size_t prelen = 0;
+	size_t extstart = 0;
+	size_t extlen = 0;
+	bool inext = false;
+	for(auto c : fname){
+		if(!inext && c == '.'){
+			extstart = prelen;
+		}else{ 
+			if(inext) ++extlen;
+			else ++prelen;
+		}
+	}
+	auto name = fname.substr(0, prelen > 8 ? 8 : prelen);
+	if(extlen){
+		auto ext = fname.substr(extstart, extlen > 3 ? 3 : extlen);
+		name += ext;
+	}
+	return name;
+}
+
+static bool in_string(char c, const string &s){
+	for(auto sc : s) if(sc == c) return true;
+	return false;
+}
+
+static string filter_chars(string path){
+	const string allowed_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_.+";
+	const char replacement = '_';
+	for(auto &pc : path){
+		if(pc >= 'a' && pc <= 'z') pc += ('A' - 'a');
+		if(!in_string(pc, allowed_chars)) pc = replacement;
+	}
+	return path;
+}
+
+static bt_filesize_t dual_to_le(l9660_duint32 dual){
+	return *(bt_filesize_t*)dual.le;
+}
+
+static l9660_dir *get_parent(l9660_fs *fs, const fs_path &path){
+	l9660_dir *parent = new l9660_dir();
+	l9660_fs_open_root(parent, fs);
+	for(auto p : path){
+		l9660_dir *newparent = new l9660_dir();
+		auto px = shorten(filter_chars(p));
+		auto res = l9660_opendirat(newparent, parent, p.c_str());
+		delete parent;
+		if(res != L9660_OK){
+			delete newparent;
+			return nullptr;
+		}
+		parent = newparent;
+	}
+	return parent;
+}
+
+bool is_path_root(const fs_path &path){
+	return (path.empty() || path[0].empty());
+}
+
 ILock *iso9660_lock;
 
 class ISOFileHandle : public IFileHandle{
@@ -17,6 +97,12 @@ private:
 	l9660_file *file;
 	FilesystemNodePointer node;
 public:
+	ISOFileHandle(l9660_file *f) : file(f) {}
+
+	~ISOFileHandle(){
+		delete file;
+	}
+
 	size_t Read(size_t bytes, char *buffer) override{
 		if(file){
 			auto hl = iso9660_lock->LockExclusive();
@@ -66,13 +152,75 @@ public:
 	FilesystemNodePointer GetNode() override{
 		return node;
 	}
+
+
+	void Close() override{}
+	bool Wait() override{
+		return false;
+	}
+};
+
+class ISOFilesystemNode : public IFilesystemNode{
+private:
+	l9660_dir parent;
+	char name[BT_MAX_SEGLEN + 1] = {0};
+	bt_filesize_t size = 0;
+	fs_item_types type = fs_item_types::FS_Invalid;
+
+	bool root;
+public:
+	ISOFilesystemNode(l9660_dir *p, const char *n, bt_filesize_t s, fs_item_types t, bool r = false) : parent(*p), size(s), type(t), root(r){
+		strncpy(name, n, BT_MAX_SEGLEN);
+		replace_char(name, ';', '\0');
+	}
+
+	IFileHandle *OpenFile(uint32_t mode) override{
+		if(type == FS_File){
+			auto file = new l9660_file();
+			auto status = l9660_openat(file, &parent, name);
+			if(status != L9660_OK){
+				delete file;
+				return nullptr;
+			}
+			return new ISOFileHandle(file);
+		}
+		return nullptr;
+	}
+	IDirectoryHandle *OpenDirectory(uint32_t mode) override;
+
+	const char *GetName() override{
+		return name;
+	}
+
+	void Rename(const char *newName) override{}
+
+	bt_filesize_t GetSize() override{
+		return size;
+	}
+
+	fs_item_types GetType() override{
+		return type;
+	}
 };
 
 class ISODirectoryHandle : public IDirectoryHandle{
 private:
 	l9660_dir *dir;
+	l9660_dirent *last_ent = nullptr;
+	size_t dirsize = 0;
+
+	FilesystemNodePointer node;
 public:
-	fs_item_types ReadType() override{}
+	ISODirectoryHandle(l9660_dir *d, IFilesystemNode *n) : dir(d), node(n) {}
+
+	~ISODirectoryHandle(){
+		delete dir;
+	}
+
+	fs_item_types ReadType() override{
+		if(!last_ent) return FS_Invalid;
+		return (last_ent->flags & 0x02) ? FS_Directory : FS_File;
+	}
 
 	FilesystemNodePointer Read() override{
 		if(dir){
@@ -85,92 +233,124 @@ public:
 			}while(pent && (pent->name[0] == '\0' || pent->name[0] == '\1'));
 			if(pent){
 				l9660_dirent &ent = *pent;
-				ret.valid=true;
 				dbgpf("ISO9660: Filename '%s' Length: %i\n", ent.name, ent.name_len);
-				memset(ret.filename, 0, BT_MAX_SEGLEN);
-				strncpy(ret.filename, ent.name, ent.name_len + 1);
-				replace_char(ret.filename, ';', '\0');
-				ret.type=(ent.flags & 0x02)?FS_Directory:FS_File;
-				ret.size=dual_to_le(ent.size);
-				ret.id=dual_to_le(ent.sector);
-			}else{
-				ret.valid = false;
+				ret = new ISOFilesystemNode(dir, ent.name, dual_to_le(ent.size), (ent.flags & 0x02) ? FS_Directory : FS_File);
+				last_ent = pent;
 			}
 			return ret;
 		}
 		return nullptr;
 	}
 	
-	bool Write(IFilesystemNode &node) override{}
-	size_t Seek(size_t pos, uint32_t flags) override{}
-	FilesystemNodePointer GetNode() override{}
+	bool Write(IFilesystemNode &node) override{
+		return false;
+	}
+
+	size_t Seek(size_t pos, uint32_t flags) override{
+		auto cpos = l9660_telldir(dir);
+		if(dirsize == 0){
+			l9660_seekdir(dir, 0);
+			l9660_dirent *pent;
+			while(l9660_readdir(dir, &pent) == L9660_OK) ++dirsize;
+		}
+		auto npos = FSHelpers::SeekPosition(pos, flags, cpos, dirsize);
+		l9660_seekdir(dir, npos);
+		return npos;
+	}
+
+	FilesystemNodePointer GetNode() override{
+		return node;
+	}
 
 	void Close() override{}
-	bool Wait() override{}
+	bool Wait() override{
+		return false;
+	}
 };
 
-class ISOFilesystemNode : public IFilesystemNode{
-private:
-public:
-	IFileHandle *OpenFile(uint32_t mode) override{}
-	IDirectoryHandle *OpenDirectory(uint32_t mode) override{}
-
-	const char *GetName() override{}
-	void Rename(const char *newName) override{}
-	bt_filesize_t GetSize() override{}
-	fs_item_types GetType() override{}
-};
+IDirectoryHandle *ISOFilesystemNode::OpenDirectory(uint32_t mode){
+	if(type == FS_Directory){
+		auto dir = new l9660_dir();
+		if(root){
+			return new ISODirectoryHandle(&parent, this);
+		}else{
+			auto status = l9660_opendirat(dir, &parent, name);
+			if(status != L9660_OK){
+				delete dir;
+				return nullptr;
+			}
+			return new ISODirectoryHandle(dir, this);
+		}
+	}
+	return nullptr;
+}
 
 class ISOMountedFilesystem : public IMountedFilesystem{
 private:
-	l9660_fs fs;
-	IFileHandle *fh;
+	struct DataStruct{
+		l9660_fs fs; // Has to be first.
+		ISOMountedFilesystem *thisPtr;
+		IFileHandle *fh;
+	} data;
 public:
-	FilesystemNodePointer GetNode(const char *path) override{}
+	ISOMountedFilesystem(FilesystemNodePointer node){
+		data.thisPtr = this;
+		data.fh = node->OpenFile(FS_Read);
+		if(data.fh){
+			auto status = l9660_openfs(&data.fs, [](struct l9660_fs *fs, void *buf, uint32_t sector) -> bool{
+				auto d = (DataStruct*)fs;
+				size_t readaddr = sector * block_size;
+				auto pos = d->fh->Seek(readaddr, FS_Set);
+				if(pos != readaddr) panic("(ISO9660) Seek failure.");
+				auto ret = d->fh->Read(block_size, (char*)buf);
+				if(ret == 0) panic("(ISO9660) Failed to read sector.");
+				return (bool)ret;
+			});
+			if(status != L9660_OK) data.fh->Close();
+		}
+	}
+
+	FilesystemNodePointer GetNode(const char *path) override{
+		auto fsPath = split_path(path);
+		if(is_path_root(fsPath)){
+			auto dir = new l9660_dir();
+			l9660_fs_open_root(dir, &data.fs);
+			return new ISOFilesystemNode(dir, "", 0, FS_Directory);
+		}else{
+			scoped_ptr<l9660_dir> parent = get_parent(&data.fs, fsPath);
+			scoped_ptr<l9660_file> file = new l9660_file();
+			if(parent){
+				auto status = l9660_openat(file.get(), parent.get(), fsPath.back().c_str());
+				if(status == L9660_OK){
+					l9660_seek(file.get(), L9660_SEEK_END, 0);
+					auto size = l9660_tell(file.get());
+					return new ISOFilesystemNode(parent.get(), fsPath.back().c_str(), size, FS_File);
+				}else if(status == L9660_ENOTFILE){
+					return new ISOFilesystemNode(parent.get(), fsPath.back().c_str(), 0, FS_Directory);
+				}
+			}
+		}
+		return nullptr;
+	}
 	void Flush() override{}
-	bool Unmount() override{}
+	bool Unmount() override{
+		data.fh->Close();
+		delete data.fh;
+		data.fh = nullptr;
+		return true;
+	}
 };
 
 class ISOFilesystem : public IFilesystem{
 public:
-	IMountedFilesystem *Mount(FilesystemNodePointer node) override{}
-	bool Format(const IFilesystemNode &node, void *options) override{}
+	IMountedFilesystem *Mount(FilesystemNodePointer node) override{
+		return new ISOMountedFilesystem(node);
+	}
+
+	bool Format(const IFilesystemNode &node, void *options) override{
+		return false;
+	}
 };
-
-static bool in_string(char c, const string &s){
-	for(auto sc : s) if(sc == c) return true;
-	return false;
-}
-
-static string shorten(const string &fname){
-	size_t prelen = 0;
-	size_t extstart = 0;
-	size_t extlen = 0;
-	bool inext = false;
-	for(auto c : fname){
-		if(!inext && c == '.'){
-			extstart = prelen;
-		}else{ 
-			if(inext) ++extlen;
-			else ++prelen;
-		}
-	}
-	auto name = fname.substr(0, prelen > 8 ? 8 : prelen);
-	if(extlen){
-		auto ext = fname.substr(extstart, extlen > 3 ? 3 : extlen);
-		name += ext;
-	}
-	return name;
-}
-
-static void filter_chars(string &path){
-	const string allowed_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_.+";
-	const char replacement = '_';
-	for(auto &pc : path){
-		if(pc >= 'a' && pc <= 'z') pc += ('A' - 'a');
-		if(!in_string(pc, allowed_chars)) pc = replacement;
-	}
-}
 
 size_t strlen(const char *s) {
 	size_t i;
@@ -198,258 +378,7 @@ string convert_path(const string &path){
 	return ret;
 }
 
-string path_leaf(const string &path){
-	string ret;
-	for(auto c : path){
-		if(c == '/') ret.clear();
-		else ret += c;
-	}
-	return ret;
-}
-
-l9660_dir *get_parent(iso9660_mountdata *mount, fs_path *path){
-	l9660_dir *parent = new l9660_dir();
-	l9660_fs_open_root(parent, &mount->fs);
-	while(path && path->str && path->next){
-		l9660_dir *newparent = new l9660_dir();
-		filter_chars(path->str);
-		shorten(path->str);
-		l9660_opendirat(newparent, parent, path->str);
-		delete parent;
-		parent = newparent;
-		path = path->next;
-	}
-	return parent;
-}
-
-bool is_path_root(fs_path *path){
-	if (!path->next && strcmp(path->str, "") == 0) return true;
-	if(path->next) return false;
-	char *c = path->str;
-	while(c && *c){
-		if(*c == '/') *c = '\0';
-		++c;
-	}
-	if(strcmp(path->str, "") == 0) return true;
-	return false;
-}
-
-uint32_t dual_to_le(l9660_duint32 dual){
-	return *(uint32_t*)dual.le;
-}
-
-bool read_sector(struct l9660_fs *fs, void *buf, uint32_t sector){
-	iso9660_mountdata *mount = (iso9660_mountdata*)fs;
-	size_t readaddr = sector * block_size;
-	take_lock_recursive(&iso9660_lock);
-	bt_filesize_t pos = fseek(mount->fh, readaddr, false);
-	if(pos != readaddr) panic("(ISO9660) Seek failure.");
-	int ret=fread(mount->fh, block_size, (char*)buf);
-	release_lock(&iso9660_lock);
-	if(ret == 0) panic("(ISO9660) read_sector failed.");
-	return !!ret;
-}
-
-void *iso9660_mount(char *path){
-	void *ret = NULL;
-	take_lock(&iso9660_lock);
-	file_handle *fh = fopen(path, FS_Read);
-	if(fh){
-		iso9660_mountdata *mount = new iso9660_mountdata();
-		mount->fh = fh;
-		l9660_status s = l9660_openfs(&mount->fs, &read_sector);
-		if(s == L9660_OK){
-			ret = (void*)mount;
-		}else{
-			dbgpf("ISO9660: Failed to mount %s (status: %i)\n", path, s);
-			delete mount;
-		}
-	}
-	release_lock(&iso9660_lock);
-	return ret;
-}
-
-bool iso9660_unmount(void *mountdata){
-	iso9660_mountdata *mount = (iso9660_mountdata*)mountdata;
-	if(mount){
-		fclose(mount->fh);
-		delete mount;
-		return true;
-	}else return false;
-}
-
-void *iso9660_open(void *mountdata, fs_path *path, fs_mode_flags flags){
-	iso9660_mountdata *mount = (iso9660_mountdata*)mountdata;
-	if(mount){
-		if((flags & FS_Write) || (flags & FS_Truncate))	return NULL;
-		take_lock(&iso9660_lock);
-		scoped_ptr<l9660_dir> parent(get_parent(mount, path));
-		l9660_file *file = new l9660_file();
-		char *fname = fs_path_last_part(path)->str;
-		filter_chars(fname);
-		shorten(fname);
-		l9660_status status = l9660_openat(file, parent.get(), fname);
-		if(status == L9660_OK){
-			if((flags & FS_AtEnd)) l9660_seek(file, L9660_SEEK_END, 0);
-		}else{
-			delete file;
-			file = NULL;
-		}
-		release_lock(&iso9660_lock);
-		return (void*)file;
-	}
-	return NULL;
-}
-
-bool iso9660_close(void *filedata){
-	l9660_file *file = (l9660_file*)filedata;
-	if(file) { 
-		delete file;
-		return true;
-	}
-	return false;
-}
-
-void *iso9660_open_dir(void *mountdata, fs_path *path, fs_mode_flags flags){
-	iso9660_mountdata *mount = (iso9660_mountdata*)mountdata;
-	if(mount){
-		if((flags & FS_Write) || (flags & FS_Truncate))	return NULL;
-		take_lock(&iso9660_lock);
-		char *pathstr = path_buffer;
-		fs_path_to_string(path, pathstr);
-		l9660_dir *dir = new l9660_dir();
-		l9660_status status;
-		if(is_path_root(path)){
-			dbgout("ISO9660: Opening root directory.\n");
-			status = l9660_fs_open_root(dir, &mount->fs);
-		}else{
-			dbgpf("ISO9660: Opening directory: '%s'\n", pathstr);
-			scoped_ptr<l9660_dir> parent(get_parent(mount, path));
-			status = l9660_opendirat(dir, parent.get(), fs_path_last_part(path)->str);
-		}
-		if(status != L9660_OK){
-			delete dir;
-			dir = NULL;
-		}
-		release_lock(&iso9660_lock);
-		dbgpf("ISO9660: Returning %p\n", dir);
-		return dir;
-	}
-	return NULL;
-}
-
-bool iso9660_close_dir(void *dirdata){
-	l9660_dir *dir = (l9660_dir*)dirdata;
-	if(dir){
-		delete dir;
-		return true;
-	}
-	return false;
-}
-
-directory_entry iso9660_read_dir(void *dirdata){
-	l9660_dir *dir = (l9660_dir*)dirdata;
-	if(dir){
-		dbgout("ISO9660: Reading directory.\n");
-		directory_entry ret;
-		take_lock(&iso9660_lock);
-		l9660_dirent *pent;
-		do{
-			l9660_readdir(dir, &pent);
-		}while(pent && (pent->name[0] == '\0' || pent->name[0] == '\1'));
-		if(pent){
-			l9660_dirent &ent = *pent;
-			ret.valid=true;
-			dbgpf("ISO9660: Filename '%s' Length: %i\n", ent.name, ent.name_len);
-			memset(ret.filename, 0, BT_MAX_SEGLEN);
-			strncpy(ret.filename, ent.name, ent.name_len + 1);
-			replace_char(ret.filename, ';', '\0');
-			ret.type=(ent.flags & 0x02)?FS_Directory:FS_File;
-			ret.size=dual_to_le(ent.size);
-			ret.id=dual_to_le(ent.sector);
-		}else{
-			ret.valid = false;
-		}
-		release_lock(&iso9660_lock);
-		return ret;
-	}
-	return directory_entry();
-}
-
-bool iso9660_write_dir(void *, directory_entry){
-	return false;
-}
-
-size_t iso9660_dirseek(void *dirdata, size_t pos, uint32_t flags){
-	l9660_dir *dir = (l9660_dir*)dirdata;
-	if(dir){
-		take_lock(&iso9660_lock);
-		int32_t offset = pos;
-		int whence = L9660_SEEK_SET;
-		if((flags & FS_Backwards)) pos = -pos;
-		if((flags & FS_Relative)) whence = L9660_SEEK_CUR;
-		l9660_seek(&dir->file, whence, offset);
-		release_lock(&iso9660_lock);
-		return l9660_tell(&dir->file);
-	}
+int module_main(char *){
+	API->GetFilesystemManager().RegisterFilesystem("ISO9660", new ISOFilesystem());
 	return 0;
-}
-
-directory_entry iso9660_stat(void *mountdata, fs_path *path){
-	iso9660_mountdata *mount = (iso9660_mountdata*)mountdata;
-	if(mount){
-		directory_entry ret;
-		scoped_ptr<l9660_file> file(new l9660_file());
-		take_lock(&iso9660_lock);
-		char *pathstr = path_buffer;
-		fs_path_to_string(path, pathstr);
-		dbgpf("ISO9660: Stat '%s'\n", pathstr);
-		if(is_path_root(path)) dbgout("ISO9660: Root.\n");
-		scoped_ptr<l9660_dir> root(new l9660_dir());
-		l9660_status status;
-		l9660_fs_open_root(root.get(), &mount->fs);
-		if(!is_path_root(path)){
-			scoped_ptr<l9660_dir> parent(get_parent(mount, path));
-			status = l9660_openat(file.get(), parent.get(), fs_path_last_part(path)->str);
-		}
-		else status = L9660_ENOTFILE;
-		dbgpf("ISO9660: Stat staus %i\n", status);
-		if(status == L9660_OK || status == L9660_ENOTFILE){
-			ret.valid = true;
-			fs_path *lastpart=fs_path_last_part(path);
-			strncpy(ret.filename, lastpart->str, BT_MAX_SEGLEN);
-			if(status == L9660_OK){
-				ret.type = FS_File;
-				l9660_seek(file.get(), L9660_SEEK_END, 0);
-				ret.size = l9660_tell(file.get());
-				ret.id = file->first_sector;
-			}else{
-				ret.type = FS_Directory;
-				ret.size = 0;
-				ret.id = 0;
-			}
-		}else{
-			ret.valid = false;
-			ret.type = FS_Invalid;
-		}
-		release_lock(&iso9660_lock);
-		return ret;
-	}
-	return directory_entry();
-}
-
-bool iso9660_format(char *, void *){
-	return false;
-}
-
-fs_driver iso9660_driver = {true, "iso9660", false, iso9660_mount, iso9660_unmount,
-							iso9660_open, iso9660_close, iso9660_read, iso9660_write, iso9660_seek, iso9660_setsize, iso9660_ioctl, iso9660_flush,
-							iso9660_open_dir, iso9660_close_dir, iso9660_read_dir, iso9660_write_dir, iso9660_dirseek,
-							iso9660_stat, iso9660_format};
-
-
-int module_main(char *params){
-		init_lock(&iso9660_lock);
-		add_filesystem(&iso9660_driver);
-		return 0;
 }
