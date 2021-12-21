e8b4dedd2f21ad2f84c940b6523d45eca56f7482
Stuart Brockman <stuartbrockman@gmail.com>
2021-12-21 23:37:29 +0000

    Kernel function<T> cannot allocate!

--- src/include/module/utils/function.hpp
+++ src/include/module/utils/function.hpp
@@ -11,35 +11,74 @@ class function<ReturnValue(Args...)> {
 private:
 	typedef function<ReturnValue(Args...)> this_t;
 	typedef ReturnValue(*fnPtr)(Args...);
-public:
-	template<typename T> function(T t) : callable_(make_unique<CallableT<T>>(t)){
+
+		class ICallable {
+	public:
+		virtual ~ICallable() = default;
+		virtual ReturnValue Invoke(Args...) const = 0;
+		virtual ICallable *Clone(char *buffer) const = 0;
+	};
+
+	template <typename T>
+	class CallableT : public ICallable {
+	public:
+		CallableT(T& t)
+			: t_(t) {
+		}
+
+		~CallableT() override = default;
+
+		ReturnValue Invoke(Args... args) const override {
+			return t_(args...);
+		}
+
+		ICallable *Clone(char *buffer) const{
+			return new(buffer) CallableT<T>(*this);
+		}
+
+	private:
+		T &t_;
+	};
+
+	static constexpr size_t callableBufferSize = 32;
+	char callableBuffer[callableBufferSize];
+
+	template<typename T> ICallable *MakeCallable(T &t){
+		static_assert(sizeof(CallableT<T>) <= callableBufferSize, "Callable object must be small!");
+		return new(callableBuffer) CallableT<T>(t);
 	}
 
-	function(fnPtr ptr) : callable_(make_unique<CallableT<fnPtr>>(ptr)) {}
+	ICallable *callable_ = nullptr;
+public:
+	template<typename T> function(T t) : callable_(MakeCallable(t)){
+	}
+
+	function(fnPtr ptr) : callable_(MakeCallable(ptr)) {}
 
 	function(std::nullptr_t) {}
 
 	function() : function(nullptr) {}
 
 	function(const this_t &other){
-		if(other.callable_) callable_.reset(other.callable_->Clone());
+		if(other.callable_) callable_= other.callable_->Clone(callableBuffer);
 	}
 
 	template <typename T>
 	function& operator=(T t) {
-		callable_ = make_unique<CallableT<T>>(t);
+		callable_ = MakeCallable(t);
 		return *this;
 	}
 
 	function &operator=(std::nullptr_t){
-		callable_.reset(nullptr);
+		if(callable_) callable_->~ICallable();
+		callable_ = nullptr;
 		return *this;
 	}
 
 	function &operator=(const this_t &other){
 		if(this != &other){
-			if(other.callable_) callable_.reset(other.callable_->Clone());
-			else callable_.reset(nullptr);
+			if(other.callable_) callable_ = other.callable_->Clone(callableBuffer);
+			else callable_= nullptr;
 		}
 		return *this;
 	}
@@ -53,36 +92,9 @@ public:
 		return callable_;
 	}
 
-private:
-	class ICallable {
-	public:
-		virtual ~ICallable() = default;
-		virtual ReturnValue Invoke(Args...) const = 0;
-		virtual ICallable *Clone() const = 0;
-	};
-
-	template <typename T>
-	class CallableT : public ICallable {
-	public:
-		CallableT(const T& t)
-			: t_(t) {
-		}
-
-		~CallableT() override = default;
-
-		ReturnValue Invoke(Args... args) const override {
-			return t_(args...);
-		}
-
-		ICallable *Clone() const{
-			return new CallableT<T>(*this);
-		}
-
-	private:
-		T t_;
-	};
-
-	unique_ptr<ICallable> callable_;
+	~function(){
+		if(callable_) callable_->~ICallable();
+	}
 };
 
 #endif
