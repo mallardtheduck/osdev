810c50d46aabc1fcb71f72fae1d6b6f665baab6b
Stuart Brockman <stuartbrockman@gmail.com>
2022-11-04 20:16:17 +0000

    Fix backwards relative seeks.

--- src/include/module/utils/fshelpers.hpp
+++ src/include/module/utils/fshelpers.hpp
@@ -3,11 +3,10 @@
 
 namespace FSHelpers{
 	inline uint64_t SeekPosition(size_t pos, uint32_t flags, uint64_t cpos, uint64_t size){
-		if(flags & FS_Relative){
+		if(flags == FS_Relative){
 			if(pos > size - cpos) cpos = size;
 			else cpos += pos;
-		}
-		else if (flags == FS_Backwards){
+		}else if (flags == FS_Backwards){
 			if(pos > size) cpos = 0;
 			else cpos = size - pos;
 		}else if(flags == (FS_Relative | FS_Backwards)){
